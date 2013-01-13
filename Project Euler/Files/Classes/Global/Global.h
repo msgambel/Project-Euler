b9dfef86fe1384da29a29200676723ad41260ddd
Michael <michaelsgam10@hotmail.com>
2013-01-13 15:10:31 -0500

    Added the solution to Question 13.

--- Project Euler/Files/Classes/Global/Global.h
+++ Project Euler/Files/Classes/Global/Global.h
@@ -7,7 +7,7 @@
 #define IBOutletCollection(ClassName)
 #endif
 
-#define TotalNumberSolved 12
+#define TotalNumberSolved 13
 #define NumberOfButtonsInQuestionCell 5
 
 #define MaxSizeOfSieveOfAtkinson 500000
---
+++ Project Euler/Files/Classes/Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question13.h
@@ -0,0 +1,9 @@
+//  Question13.h
+
+#import "QuestionAndAnswer.h"
+
+@interface Question13 : QuestionAndAnswer {
+  
+}
+
+@end
