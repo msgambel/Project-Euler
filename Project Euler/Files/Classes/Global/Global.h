cc1ee432adbb482835e6f7c7e8c58b71feaadc3c
Michael <michaelsgam10@hotmail.com>
2013-02-13 13:54:52 -0500

    Added the solution to Question 44.

--- Project Euler/Files/Classes/Global/Global.h
+++ Project Euler/Files/Classes/Global/Global.h
@@ -7,7 +7,7 @@
 #define IBOutletCollection(ClassName)
 #endif
 
-#define TotalNumberSolved 43
+#define TotalNumberSolved 44
 #define NumberOfButtonsInQuestionCell 5
 
 #define MaxSizeOfSieveOfAtkinson 500000
---
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question44.h
@@ -0,0 +1,9 @@
+//  Question44.h
+
+#import "QuestionAndAnswer.h"
+
+@interface Question44 : QuestionAndAnswer {
+  
+}
+
+@end
