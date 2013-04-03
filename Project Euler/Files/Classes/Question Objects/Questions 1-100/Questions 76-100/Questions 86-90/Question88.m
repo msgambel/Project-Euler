90a168c389e889fdc197b305e22d976d14fc9d47
Michael <michaelsgam10@hotmail.com>
2013-04-02 23:27:29 -0400

    Added the solution to Question 88.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question88.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question88.m
@@ -1,7 +1,6 @@
 //  Question88.m
 
 #import "Question88.h"
-#import "Global.h"
 
 @implementation Question88
 
@@ -16,10 +15,10 @@
   self.date = @"04 February 2005";
   self.text = @"A natural number, N, that can be written as the sum and product of a given set of at least two natural numbers, {a1, a2, ... , ak} is called a product-sum number: N = a1 + a2 + ... + ak = a1  a2  ...  ak.\n\nFor example, 6 = 1 + 2 + 3 = 1 x 2 x 3.\n\nFor a given set of size, k, we shall call the smallest N with this property a minimal product-sum number. The minimal product-sum numbers for sets of size, k = 2, 3, 4, 5, and 6 are as follows.\n\nk=2: 4 = 2 x 2 = 2 + 2\nk=3: 6 = 1 x 2 x 3 = 1 + 2 + 3\nk=4: 8 = 1 x 1 x 2 x 4 = 1 + 1 + 2 + 4\nk=5: 8 = 1 x 1 x 2 x 2  2 = 1 + 1 + 2 + 2 + 2\nk=6: 12 = 1 x 1 x 1 x 1 x 2 x 6 = 1 + 1 + 1 + 1 + 2 + 6\n\nHence for 2 <= k <= 6, the sum of all the minimal product-sum numbers is 4+6+8+12 = 30; note that 8 is only counted once in the sum.\n\nIn fact, as the complete set of minimal product-sum numbers for 2 <= k <= 12 is {4, 6, 8, 12, 15, 16}, the sum is 61.\n\nWhat is the sum of all the minimal product-sum numbers for 2 <= k <= 12000?";
   self.title = @"Product-sum numbers";
-  self.answer = @"";
+  self.answer = @"7587457";
   self.number = @"Problem 88";
-  self.estimatedComputationTime = @"";
-  self.estimatedBruteForceComputationTime = @"";
+  self.estimatedComputationTime = @"0.881";
+  self.estimatedBruteForceComputationTime = @"0.881";
 }
 
 #pragma mark - Methods
@@ -33,157 +32,83 @@
   
   // Here, we simply
   
-  // Variable to hold the sum. Default the sum to 0.
-  uint sum = 0;
+  const uint maxSize = 12000;
   
-  // Set the max size for the Amicable numbers.
-  uint maxSize = 24000;
+  BOOL shouldAddNumber = NO;
   
-  // Variable to hold the current prime number form the prime numbers array.
-  uint primeNumber = 0;
+  uint k = 0;
   
-  // Variable to hold the current number for use when factoring the number.
+  uint sumOfFactors = 0;
+  
   uint currentNumber = 0;
   
-  // Variable to hold the sum of the divisors for every number up to the max size.
-  uint sumOfDivisors = 0;
+  uint currentSquareRoot = 0;
   
-  // Variable used to compute the maximum number of prime factors a number less
-  // than the max size can have.
-  uint currentMaxSize = 1;
+  uint totalMinimalProductSum = 4;
   
-  // Variable to hold the index of the current prime power struct when factoring
-  // the current number.
-  uint primePowerIndex = 0;
+  uint minimalProductSums[(maxSize + 1)] = {0};
   
-  // Variable to hold the square root of the max size, used to minimize computations.
-  uint sqrtOfCurrentNumber = ((uint)sqrt(maxSize));
+  minimalProductSums[2] = 4;
   
-  // Variable to hold the maximum number of prime factors a number less than the
-  // max size can have.
-  uint maxNumberOfPrimeFactors = 1;
+  NSMutableArray * sumOfFactorsArray = [[NSMutableArray alloc] initWithCapacity:maxSize];
   
-  // Variable array to hold the abundent numbers up to the max size.
-  uint minimalProductSumNumbers[maxSize];
+  NSMutableArray * factorsForNumber = nil;
   
-  // Array that holds the primes numbers. The method is defined in the super class.
-  NSMutableArray * primeNumbersArray = [self arrayOfPrimeNumbersLessThan:sqrtOfCurrentNumber];
+  NSMutableArray * currentFactorsForNumber = [[NSMutableArray alloc] initWithObjects:[NSNumber numberWithInt:0], nil];
   
-  // For all the prime numbers in the prime numbers array,
-  for(int currentPrimeNumber = 0; currentPrimeNumber < [primeNumbersArray count]; currentPrimeNumber++){
-    // Grab the current prime number from the prime numbers array.
-    primeNumber = [[primeNumbersArray objectAtIndex:currentPrimeNumber] intValue];
-    
-    // Multiply the current max size by the current prime number.
-    currentMaxSize *= primeNumber;
-    
-    // Increment the maximum number of prime factors by 1.
-    maxNumberOfPrimeFactors++;
-    
-    // If the current max size is greater than the max size,
-    if(currentMaxSize > maxSize){
-      // Break out of the loop.
-      break;
+  [sumOfFactorsArray addObject:currentFactorsForNumber];
+  
+  for(int i = 0; i < 3; i++){
+    currentFactorsForNumber = [[NSMutableArray alloc] initWithObjects:[NSNumber numberWithInt:i], nil];
+    [sumOfFactorsArray addObject:currentFactorsForNumber];
+  }
+  currentFactorsForNumber = [[NSMutableArray alloc] initWithObjects:[NSNumber numberWithInt:2], [NSNumber numberWithInt:3], nil];
+  
+  [sumOfFactorsArray addObject:currentFactorsForNumber];
+  
+  for(int n = 5; n <= (1.1 * maxSize); n++){
+    if([self isPrime:n]){
+      currentFactorsForNumber = [[NSMutableArray alloc] initWithObjects:[NSNumber numberWithInt:(n-1)], nil];
     }
-  }
-  // Variable to hold the current prime power struct when factoring the current
-  // number.
-  PrimePower currentPrimePower = PrimePowerZero;
-  
-  // Variable to hold the current prime power structs when factoring the current
-  // number.
-  PrimePower currentPrimePowers[maxNumberOfPrimeFactors];
-  
-  // For all the prime factors up to the maximum number of prime factors,
-  for(int i = 0; i < maxNumberOfPrimeFactors; i++){
-    // Set the current prime factors to zero by default.
-    currentPrimePowers[i] = PrimePowerZero;
-  }
-  // For all the numbers from 4 to the max size,
-  for(int number = 4; number < maxSize; number++){
-    if([self isPrime:number] == NO){
-      // Grab the current number for use in factoring.
-      currentNumber = number;
+    else{
+      shouldAddNumber = NO;
+      currentNumber = n;
+      currentSquareRoot = ((uint)sqrt((double)n));
+      currentFactorsForNumber = [[NSMutableArray alloc] init];
       
-      // Recompute the square root of the current minimum number, in order to
-      // speed up the computation.
-      sqrtOfCurrentNumber = ((uint)sqrt(currentNumber));
-      
-      // Reset the prime power index to 0, as we haven't found any prime factors
-      // yet!
-      primePowerIndex = 0;
-      
-      // For all the prime numbers in the prime numbers array,
-      for(int currentPrimeNumber = 0; currentPrimeNumber < [primeNumbersArray count]; currentPrimeNumber++){
-        // Grab the current prime number from the prime numbers array.
-        primeNumber = [[primeNumbersArray objectAtIndex:currentPrimeNumber] intValue];
-        
-        // If the current prime number is less than or equal to the square root of
-        // the current number,
-        if(primeNumber <= sqrtOfCurrentNumber){
-          // If the current prime number divides the current number,
-          if((currentNumber % primeNumber) == 0){
-            // Set the current prime power to be the prime number.
-            currentPrimePower = PrimePowerMake(primeNumber, 0);
+      for(int factor = 2; factor <= currentSquareRoot; factor++){
+        if((n % factor) == 0){
+          currentNumber = n / factor;
+          
+          factorsForNumber = [sumOfFactorsArray objectAtIndex:currentNumber];
+          
+          for(NSNumber * number in factorsForNumber){
+            sumOfFactors = [number intValue] + (factor - 1);
             
-            // While the current prime number divides the current number,
-            while((currentNumber % primeNumber) == 0){
-              // Divide out the prime number from the number.
-              currentNumber /= primeNumber;
+            if(n > sumOfFactors){
+              k = n - sumOfFactors;
               
-              // Increment the current prime powers power by 1.
-              currentPrimePower.power++;
+              if(k <= maxSize){
+                if(minimalProductSums[k] == 0){
+                  minimalProductSums[k] = n;
+                  shouldAddNumber = YES;
+                }
+                
+              }
+              [currentFactorsForNumber addObject:[NSNumber numberWithInt:sumOfFactors]];
             }
-            // Recompute the square root of the current number, in order to speed
-            // up the computation.
-            sqrtOfCurrentNumber = ((uint)sqrt(currentNumber));
-            
-            // Set the current prime power to the current prime powers index.
-            currentPrimePowers[primePowerIndex] = currentPrimePower;
-            
-            // Increment the current prime power index by 1.
-            primePowerIndex++;
           }
         }
-        // If the current prime number is greater than or equal to the square root
-        // of the current prime number,
-        else{
-          // Break out of the loop.
-          break;
-        }
       }
-      // If the current minimum number is NOT equal to 1 after the above factoring,
-      // it must be a prime number itself,
-      if(currentNumber > 1){
-        // Set the current prime number (which is the current number) to the current
-        // prime power. Since this number is prime, there is no need to loop to
-        // find the power, as it must be 1.
-        currentPrimePower = PrimePowerMake(currentNumber, 1);
-        
-        // Set the current prime power to be the prime number.
-        currentPrimePowers[primePowerIndex] = currentPrimePower;
-        
-        // Increment the current prime power index by 1.
-        primePowerIndex++;
+      if(shouldAddNumber){
+        totalMinimalProductSum += n;
       }
-      // Set the sum of the divisors for this number to 1.
-      sumOfDivisors = 1;
-      
-      // TODO: Figure all combinations of factors, and add (n - k) ones onto the
-      //       k factors to arrive at n. If p_1 * ... * p_k = n, then n is the
-      //       minimal product sum for (p_1 + ... + p_k) + (p_1 * ... * p_k),
-      //       where (p_1 + ... + p_k) = 1 + ... + 1.
-      
-      // TODO: Store all the combinations of factors for each number in an array
-      //       and use those combinations when factoring larger numbers to get
-      //       the total sums. Potentially use an NSMutableArray?
-      
-      // TODO: Don't think we need the prime factorization actually...
+      [currentFactorsForNumber addObject:[NSNumber numberWithInt:(n-1)]];
     }
+    [sumOfFactorsArray addObject:currentFactorsForNumber];
   }
-  
-//  // Set the answer string to the.
-//  self.answer = [NSString stringWithFormat:@"%d", ];
+  // Set the answer string to the.
+  self.answer = [NSString stringWithFormat:@"%d", totalMinimalProductSum];
   
   // Get the amount of time that has passed while the computation was happening.
   NSTimeInterval computationTime = [[NSDate date] timeIntervalSinceDate:startTime];
@@ -209,8 +134,85 @@
   // Note: This is the same algorithm as the optimal one. I can't think of a more
   //       brute force way to do this!
   
-  // Note: This is basically the same algorithm as the optimal one. The optimal
-  //       algorithm just uses better tricks for avoiding computation.
+  // Here, we simply
+  
+  const uint maxSize = 12000;
+  
+  BOOL shouldAddNumber = NO;
+  
+  uint k = 0;
+  
+  uint sumOfFactors = 0;
+  
+  uint currentNumber = 0;
+  
+  uint currentSquareRoot = 0;
+  
+  uint totalMinimalProductSum = 4;
+  
+  uint minimalProductSums[(maxSize + 1)] = {0};
+  
+  minimalProductSums[2] = 4;
+  
+  NSMutableArray * sumOfFactorsArray = [[NSMutableArray alloc] initWithCapacity:maxSize];
+  
+  NSMutableArray * factorsForNumber = nil;
+  
+  NSMutableArray * currentFactorsForNumber = [[NSMutableArray alloc] initWithObjects:[NSNumber numberWithInt:0], nil];
+  
+  [sumOfFactorsArray addObject:currentFactorsForNumber];
+  
+  for(int i = 0; i < 3; i++){
+    currentFactorsForNumber = [[NSMutableArray alloc] initWithObjects:[NSNumber numberWithInt:i], nil];
+    [sumOfFactorsArray addObject:currentFactorsForNumber];
+  }
+  currentFactorsForNumber = [[NSMutableArray alloc] initWithObjects:[NSNumber numberWithInt:2], [NSNumber numberWithInt:3], nil];
+  
+  [sumOfFactorsArray addObject:currentFactorsForNumber];
+  
+  for(int n = 5; n <= (1.1 * maxSize); n++){
+    if([self isPrime:n]){
+      currentFactorsForNumber = [[NSMutableArray alloc] initWithObjects:[NSNumber numberWithInt:(n-1)], nil];
+    }
+    else{
+      shouldAddNumber = NO;
+      currentNumber = n;
+      currentSquareRoot = ((uint)sqrt((double)n));
+      currentFactorsForNumber = [[NSMutableArray alloc] init];
+      
+      for(int factor = 2; factor <= currentSquareRoot; factor++){
+        if((n % factor) == 0){
+          currentNumber = n / factor;
+          
+          factorsForNumber = [sumOfFactorsArray objectAtIndex:currentNumber];
+          
+          for(NSNumber * number in factorsForNumber){
+            sumOfFactors = [number intValue] + (factor - 1);
+            
+            if(n > sumOfFactors){
+              k = n - sumOfFactors;
+              
+              if(k <= maxSize){
+                if(minimalProductSums[k] == 0){
+                  minimalProductSums[k] = n;
+                  shouldAddNumber = YES;
+                }
+              
+            }
+            [currentFactorsForNumber addObject:[NSNumber numberWithInt:sumOfFactors]];
+          }
+        }
+      }
+    }
+    if(shouldAddNumber){
+      totalMinimalProductSum += n;
+    }
+      [currentFactorsForNumber addObject:[NSNumber numberWithInt:(n-1)]];
+    }
+    [sumOfFactorsArray addObject:currentFactorsForNumber];
+  }
+  // Set the answer string to the.
+  self.answer = [NSString stringWithFormat:@"%d", totalMinimalProductSum];
   
   // Get the amount of time that has passed while the computation was happening.
   NSTimeInterval computationTime = [[NSDate date] timeIntervalSinceDate:startTime];
