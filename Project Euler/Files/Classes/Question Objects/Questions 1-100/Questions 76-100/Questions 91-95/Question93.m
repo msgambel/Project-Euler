2317ff6f6131cd884f3b18ab7b69526156e6af43
Michael <michaelsgam10@hotmail.com>
2013-04-09 19:29:55 -0400

    Fixed a bug in Question 93 that was not checking the correct permutations.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question93.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question93.m
@@ -4,6 +4,8 @@
 
 @interface Question93 (Private)
 
+- (void)nextPermutation:(int *)aPermutation size:(uint)aSize;
+- (void)swapIndex1:(uint)aIndex1 withIndex2:(uint)aIndex2 inArray:(int *)aArray;
 - (double)reversePolishNotation:(NSString *)aEquation;
 
 @end
@@ -23,8 +25,8 @@
   self.title = @"Arithmetic expressions";
   self.answer = @"1258";
   self.number = @"Problem 93";
-  self.estimatedComputationTime = @"2.16";
-  self.estimatedBruteForceComputationTime = @"2.16";
+  self.estimatedComputationTime = @"7.38";
+  self.estimatedBruteForceComputationTime = @"7.38";
 }
 
 #pragma mark - Methods
@@ -38,57 +40,52 @@
   
   // Here, we simply
   
+  // http://en.wikipedia.org/wiki/Reverse_Polish_notation
+  
   uint largestConsecutiveNumber = 0;
   
   double result = 0.0;
+  
+  int permutation[4] = {0};
   
   NSArray * sortedResults = nil;
   
   NSNumber * currentNumber = nil;
   
+  NSString * equation = nil;
+  
   NSString * concatenatedDigits = nil;
   
-  NSMutableArray * results = nil;
+  NSMutableArray * results = [[NSMutableArray alloc] init];
   
   // Constant array to hold all the operators.
   const NSArray * operators = [NSArray arrayWithObjects:@"+", @"-", @"*", @"/", nil];
   
   NSSortDescriptor * lowestToHighest = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES];
   
-  NSString * equation = nil;
-  
-  for(int a = 0; a < 10; a++){
+  for(int a = 1; a < 10; a++){
     for(int b = (a + 1); b < 10; b++){
       for(int c = (b + 1); c < 10; c++){
         for(int d = (c + 1); d < 10; d++){
-          results = [[NSMutableArray alloc] init];
+          [results removeAllObjects];
           
           for(NSString * operator1 in operators){
             for(NSString * operator2 in operators){
               for(NSString * operator3 in operators){
-                equation = [NSString stringWithFormat:@"%d%d%d%d%@%@%@", a, b, c, d, operator1, operator2, operator3];
-                result = [self reversePolishNotation:equation];
                 
-                if((result != 0.5) && (result > 0.0)){
-                  [results addObject:[NSNumber numberWithInt:((uint)result)]];
-                }
-                equation = [NSString stringWithFormat:@"%d%d%d%@%d%@%@", a, b, c, operator1, d, operator2, operator3];
-                result = [self reversePolishNotation:equation];
+                permutation[0] = a;
+                permutation[1] = b;
+                permutation[2] = c;
+                permutation[3] = d;
                 
-                if((result != 0.5) && (result > 0.0)){
-                  [results addObject:[NSNumber numberWithInt:((uint)result)]];
-                }
-                equation = [NSString stringWithFormat:@"%d%d%@%d%d%@%@", a, b, operator1, c, d, operator2, operator3];
-                result = [self reversePolishNotation:equation];
-                
-                if((result != 0.5) && (result > 0.0)){
-                  [results addObject:[NSNumber numberWithInt:((uint)result)]];
-                }
-                equation = [NSString stringWithFormat:@"%d%d%@%d%@%d%@", a, b, operator1, c, operator2, d, operator3];
-                result = [self reversePolishNotation:equation];
-                
-                if((result != 0.5) && (result > 0.0)){
-                  [results addObject:[NSNumber numberWithInt:((uint)result)]];
+                while(permutation[0] != 0){
+                  equation = [NSString stringWithFormat:@"%d%d%d%@%d%@%@", permutation[0], permutation[1], permutation[2], operator1, permutation[3], operator2, operator3];
+                  result = [self reversePolishNotation:equation];
+                  
+                  if((result != 0.5) && (result > 0.0)){
+                    [results addObject:[NSNumber numberWithInt:((uint)result)]];
+                  }
+                  [self nextPermutation:permutation size:4];
                 }
               }
             }
@@ -101,31 +98,57 @@
           
           for(int number = 0; number < [results count]; number++){
             currentNumber = [results objectAtIndex:number];
+          }
+          
+          for(int number = 0; number < [results count]; number++){
+            currentNumber = [results objectAtIndex:number];
             
             if([currentNumber intValue] != (number + 1)){
-              if(largestConsecutiveNumber < (number + 1)){
-                largestConsecutiveNumber = (number + 1);
-                NSLog(@"max: %d", largestConsecutiveNumber);
+              if(largestConsecutiveNumber < number){
+                largestConsecutiveNumber = number;
+                
                 concatenatedDigits = [NSString stringWithFormat:@"%d%d%d%d", a, b, c, d];
               }
+              // Break out of the loop.
               break;
             }
           }
+          // If we are no longer computing,
+          if(!_isComputing){
+            // Break out of the loop.
+            break;
+          }
+        }
+        // If we are no longer computing,
+        if(!_isComputing){
+          // Break out of the loop.
+          break;
         }
       }
+      // If we are no longer computing,
+      if(!_isComputing){
+        // Break out of the loop.
+        break;
+      }
+    }
+    // If we are no longer computing,
+    if(!_isComputing){
+      // Break out of the loop.
+      break;
     }
   }
-  
-  // Set the answer string to the
-  self.answer = concatenatedDigits;
-  
-  // Get the amount of time that has passed while the computation was happening.
-  NSTimeInterval computationTime = [[NSDate date] timeIntervalSinceDate:startTime];
-  
-  // Set the estimated computation time to the calculated value. We use scientific
-  // notation here, as the run time should be very short.
-  self.estimatedComputationTime = [NSString stringWithFormat:@"%.03g", computationTime];
-  
+  // If the user has not cancelled the computation at this point,
+  if(_isComputing){
+    // Set the answer string to the
+    self.answer = concatenatedDigits;
+    
+    // Get the amount of time that has passed while the computation was happening.
+    NSTimeInterval computationTime = [[NSDate date] timeIntervalSinceDate:startTime];
+    
+    // Set the estimated computation time to the calculated value. We use scientific
+    // notation here, as the run time should be very short.
+    self.estimatedComputationTime = [NSString stringWithFormat:@"%.03g", computationTime];
+  }
   // Tell the delegate we have finished the computation.
   [self.delegate finishedComputing];
   
@@ -165,6 +188,49 @@
 
 @implementation Question93 (Private)
 
+- (void)nextPermutation:(int *)aPermutation size:(uint)aSize; {
+  BOOL nextPermutationExists = NO;
+  
+  int index1 = aSize - 1;
+  
+  int index2 = aSize;
+  
+  for(int index = 0; index < (aSize - 1); index++){
+    if(aPermutation[index] < aPermutation[index + 1]){
+      nextPermutationExists = YES;
+      
+      break;
+    }
+  }
+  if(nextPermutationExists == NO){
+    aPermutation[0] = 0;
+    
+    return;
+  }
+  while(aPermutation[index1 - 1] >= aPermutation[index1]){
+    index1--;
+  }
+  while(aPermutation[index2 - 1] <= aPermutation[index1 - 1]){
+    index2--;
+  }
+  [self swapIndex1:(index1 - 1) withIndex2:(index2 - 1) inArray:aPermutation];
+  
+  index1++;
+  index2 = aSize;
+  
+  while(index1 < index2){
+    [self swapIndex1:(index1 - 1) withIndex2:(index2 - 1) inArray:aPermutation];
+    index1++;
+    index2--;
+  }
+}
+
+- (void)swapIndex1:(uint)aIndex1 withIndex2:(uint)aIndex2 inArray:(int *)aArray; {
+  aArray[aIndex1] += aArray[aIndex2];
+  aArray[aIndex2] = aArray[aIndex1] - aArray[aIndex2];
+  aArray[aIndex1] -= aArray[aIndex2];
+}
+
 - (double)reversePolishNotation:(NSString *)aEquation; {
   // Constant array to hold all the operators.
   const NSArray * operators = [NSArray arrayWithObjects:@"+", @"-", @"*", @"/", nil];
@@ -182,6 +248,10 @@
   
   NSString * element = nil;
   
+  NSString * leftHandSide = nil;
+  
+  NSString * rightHandSide = nil;
+  
   NSMutableArray * stack = [[NSMutableArray alloc] init];
   
   while(stringIndex < aEquation.length){
@@ -193,27 +263,26 @@
     operatorIndex = [operators indexOfObject:element];
     
     if(operatorIndex != NSNotFound){
-      NSString * lhs = [stack lastObject];
+      leftHandSide = [stack lastObject];
       [stack removeLastObject];
-      NSString * rhs = [stack lastObject];
+      rightHandSide = [stack lastObject];
       [stack removeLastObject];
-      
       switch(operatorIndex){
         case 0:
-          result = [lhs doubleValue] + [rhs doubleValue];
+          result = [leftHandSide doubleValue] + [rightHandSide doubleValue];
           break;
         case 1:
-          result = [lhs doubleValue] - [rhs doubleValue];
+          result = [leftHandSide doubleValue] - [rightHandSide doubleValue];
           break;
         case 2:
-          result = [lhs doubleValue] * [rhs doubleValue];
+          result = [leftHandSide doubleValue] * [rightHandSide doubleValue];
           break;
         case 3:
-          if([rhs doubleValue] == 0.0){
+          if([rightHandSide doubleValue] == 0.0){
             dividedByZero = YES;
           }
           else{
-            result = [lhs doubleValue] / [rhs doubleValue];
+            result = [leftHandSide doubleValue] / [rightHandSide doubleValue];
           }
           break;
         default:
