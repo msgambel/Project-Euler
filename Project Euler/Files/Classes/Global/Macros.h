2115af94f17774de8dff0b6669217a894db065ce
Michael <michaelsgam10@hotmail.com>
2013-06-15 13:48:10 -0400

    Added comments to the Macro's definitions.

--- Project Euler/Files/Classes/Global/Macros.h
+++ Project Euler/Files/Classes/Global/Macros.h
@@ -3,8 +3,11 @@
 #ifndef Project_Euler_Macros_h
 #define Project_Euler_Macros_h
 
+// Make's sure a number is within a given range, and sets it to the closest
+// endpoint if it is not.
 #define CLAMP(X, A, B) ((X < A) ? A : ((X > B) ? B : X))
 
+// Define's for the common types of polygonal numbers.
 #define TriangleNumber(n) (n * (n + 1) / 2)
 #define SquareNumber(n) (n * n)
 #define PentagonalNumber(n) (n * ((3 * n) - 1) / 2)
