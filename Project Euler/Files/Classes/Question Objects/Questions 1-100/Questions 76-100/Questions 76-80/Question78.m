7f698eb4e2aa2b4e3f45f4c523de1eb6243ef923
Michael <michaelsgam10@hotmail.com>
2013-07-04 15:08:24 -0400

    Added more keywords to Question 78.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question78.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question78.m
@@ -18,7 +18,7 @@
   self.title = @"Coin partitions";
   self.answer = @"55374";
   self.number = @"78";
-  self.keywords = @"coin,paritions,divisible,unique,piles";
+  self.keywords = @"coin,paritions,divisible,unique,piles,separated,million";
   self.estimatedComputationTime = @"0.261";
   self.estimatedBruteForceComputationTime = @"0.261";
 }
