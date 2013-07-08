044f6b73a4e7656a07fe18602d0f8d0dd4b7c1be
Michael <michaelsgam10@hotmail.com>
2013-07-08 13:08:06 -0400

    Added more keywords to Question 37.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question37.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question37.m
@@ -26,7 +26,7 @@
   self.title = @"Truncatable primes";
   self.answer = @"748317";
   self.number = @"34";
-  self.keywords = @"primes,left,right,truncatable";
+  self.keywords = @"primes,left,right,truncatable,continuously,remove,digits";
   self.estimatedComputationTime = @"0.99";
   self.estimatedBruteForceComputationTime = @"1.14";
 }
