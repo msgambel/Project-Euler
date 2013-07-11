d7863b941503cd24aeea84d9fe6edc3aad1989af
Michael <michaelsgam10@hotmail.com>
2013-07-11 14:37:38 -0400

    Added more keywords to Question 35.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question35.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question35.m
@@ -24,7 +24,7 @@
   self.title = @"Circular primes";
   self.answer = @"55";
   self.number = @"35";
-  self.keywords = @"primes,rotation,circular";
+  self.keywords = @"primes,rotation,circular,digits,million,permutation,1";
   self.estimatedComputationTime = @"0.99";
   self.estimatedBruteForceComputationTime = @"1.42";
 }
