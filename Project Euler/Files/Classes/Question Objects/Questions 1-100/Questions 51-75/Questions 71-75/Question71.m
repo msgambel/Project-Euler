f37c36ab9e7c95177b0ff403efca87d6d6a82f70
Michael <michaelsgam10@hotmail.com>
2013-09-01 14:06:27 -0400

    Added more keywords to Question 71.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question71.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question71.m
@@ -17,7 +17,7 @@
   self.title = @"Ordered fractions";
   self.answer = @"428570";
   self.number = @"71";
-  self.keywords = @"ordered,fractions,ascending,size,reduced,proper";
+  self.keywords = @"counting,fractions,reduced,proper,ascending,order,interval,positive,integers";
   self.estimatedComputationTime = @"1.87e-04";
   self.estimatedBruteForceComputationTime = @"127";
 }
