55fe28a82adb76a363b79b46fb53231561d795c3
Michael Sgambelluri <michaelsgam10@hotmail.com>
2013-11-07 11:22:53 -0500

    Added more keywords to Question 5.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question5.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question5.m
@@ -17,7 +17,7 @@
   self.title = @"Smallest multiple";
   self.answer = @"232792560";
   self.number = @"5";
-  self.keywords = @"divisible,factorial,evenly,positive,number,20,twenty";
+  self.keywords = @"divisible,factorial,evenly,smallest,positive,number,20,twenty,remainder";
   self.estimatedComputationTime = @"3.9e-05";
   self.estimatedBruteForceComputationTime = @"5.81e-05";
 }
