738da08e3bf54f5b01da0b04c0150e45b3d57278
Michael Sgambelluri <michaelsgam10@hotmail.com>
2013-12-05 14:05:49 -0500

    Added more keywords to Question 72. Also fixed the Question Text.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question72.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question72.m
@@ -14,11 +14,11 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"18 June 2004";
-  self.text = @"Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a reduced proper fraction.\n\nIf we list the set of reduced proper fractions for d  8 in ascending order of size, we get:\n\n1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8\n\nIt can be seen that there are 21 elements in this set.\n\nHow many elements would be contained in the set of reduced proper fractions for d <= 1,000,000?";
+  self.text = @"Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a reduced proper fraction.\n\nIf we list the set of reduced proper fractions for d <= 8 in ascending order of size, we get:\n\n1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8\n\nIt can be seen that there are 21 elements in this set.\n\nHow many elements would be contained in the set of reduced proper fractions for d <= 1,000,000?";
   self.title = @"Counting fractions";
   self.answer = @"303963552391";
   self.number = @"72";
-  self.keywords = @"counting,fractions,reduced,proper,ascending,order,interval,positive,integers";
+  self.keywords = @"counting,fractions,reduced,proper,ascending,order,interval,positive,integers,list,set";
   self.estimatedComputationTime = @"4.77";
   self.estimatedBruteForceComputationTime = @"4.77";
 }
