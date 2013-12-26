9f9aac0fa8272dc0d531bdc290394cf1e9fe8f94
Michael Sgambelluri <michaelsgam10@hotmail.com>
2013-12-26 15:37:55 -0500

    Added more keywords to Question 51.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question51.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question51.m
@@ -17,7 +17,7 @@
   self.title = @"Prime digit replacements";
   self.answer = @"121313";
   self.number = @"51";
-  self.keywords = @"prime,family,digit,replacement,number,eight,8";
+  self.keywords = @"prime,value,family,digit,replacements,number,eight,8,*";
   self.estimatedComputationTime = @"0.794";
   self.estimatedBruteForceComputationTime = @"0.794";
 }
