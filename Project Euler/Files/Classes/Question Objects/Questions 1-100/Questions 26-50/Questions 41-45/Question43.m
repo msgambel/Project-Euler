c0084b7ddccee3b43ac00d79da54165e4cbff455
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-01-03 14:36:41 -0500

    Added more keywords to Question 43.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question43.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question43.m
@@ -23,7 +23,7 @@
   self.title = @"Sub-string divisibility";
   self.answer = @"16695334890";
   self.number = @"43";
-  self.keywords = @"string,divisible,pandigital,sub,divisibility,sum,property";
+  self.keywords = @"string,divisible,pandigital,sub,divisibility,sum,property,digit,numbers";
   self.estimatedComputationTime = @"8.54e-02";
   self.estimatedBruteForceComputationTime = @"354";
 }
