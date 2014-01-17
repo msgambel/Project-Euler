f1871447d31b47c15394d0200b53766b27f61430
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-01-17 13:21:14 -0500

    Added more keywords to Question 29.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question29.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question29.m
@@ -19,7 +19,7 @@
   self.title = @"Distinct powers";
   self.answer = @"9183";
   self.number = @"29";
-  self.keywords = @"base,power,unique,integer,combinations,numerical,order,distinct,sequence,repeats,removed";
+  self.keywords = @"base,power,unique,integer,combinations,numerical,order,distinct,sequence,repeats,removed,terms,generated";
   self.estimatedComputationTime = @"7.52e-03";
   self.estimatedBruteForceComputationTime = @"8.77";
 }
