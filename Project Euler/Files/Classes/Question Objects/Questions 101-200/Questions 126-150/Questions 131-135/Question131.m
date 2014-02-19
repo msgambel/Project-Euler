7cff5f58bb0a7961007fa29b2622a2a4ce525ae3
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-02-19 06:53:15 -0500

    Added more keywords to Question 131.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 126-150/Questions 131-135/Question131.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 126-150/Questions 131-135/Question131.m
@@ -17,7 +17,7 @@
   self.title = @"Prime cube partnership";
   self.answer = @"173";
   self.number = @"131";
-  self.keywords = @"primes,cube,partnership,factoring,positive,integer,property,difference,of,cubes,one,million,1000000";
+  self.keywords = @"primes,cube,partnership,factoring,positive,integer,property,difference,of,cubes,one,million,1000000,value,unique";
   self.estimatedComputationTime = @"1.77e-04";
   self.estimatedBruteForceComputationTime = @"1.77e-04";
 }
