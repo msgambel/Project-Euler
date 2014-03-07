b6d7e98a5bf824877af28d1ab5e672e11cb7390e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-03-07 13:55:53 -0500

    Added more keywords to Question 88.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question88.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question88.m
@@ -17,7 +17,7 @@
   self.title = @"Product-sum numbers";
   self.answer = @"7587457";
   self.number = @"88";
-  self.keywords = @"minimal,product,sum,recursive,product,k";
+  self.keywords = @"minimal,product,sum,recursive,product,k,complete,set,product-sum";
   self.estimatedComputationTime = @"0.881";
   self.estimatedBruteForceComputationTime = @"0.881";
 }
