791e0f971941dc5c9ec20024dee64152a8ae6ad2
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-03-23 15:48:22 -0400

    Added more keywords to Question 72.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question72.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question72.m
@@ -18,7 +18,7 @@
   self.title = @"Counting fractions";
   self.answer = @"303963552391";
   self.number = @"72";
-  self.keywords = @"counting,fractions,reduced,proper,ascending,order,interval,positive,integers,list,set";
+  self.keywords = @"counting,fractions,reduced,proper,ascending,order,interval,positive,integers,list,set,elements,contained";
   self.estimatedComputationTime = @"4.77";
   self.estimatedBruteForceComputationTime = @"4.77";
 }
