5e13078ccc119f6b4352cd648d86b717998f63e6
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-03-25 17:34:06 -0400

    Added more keywords to Question 70.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question70.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question70.m
@@ -18,7 +18,7 @@
   self.title = @"Totient permutation";
   self.answer = @"8319823";
   self.number = @"70";
-  self.keywords = @"totient,function,maximum,phi,euler's,product,permutation,relatively,prime,positive,numbers,ratio,minimum";
+  self.keywords = @"totient,function,maximum,phi,euler's,eulers,product,permutation,relatively,prime,positive,numbers,ratio,produces,minimum";
   self.estimatedComputationTime = @"1.83e-02";
   self.estimatedBruteForceComputationTime = @"50.3";
 }
