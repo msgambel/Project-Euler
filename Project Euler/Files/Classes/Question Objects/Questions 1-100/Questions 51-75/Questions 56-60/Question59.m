feb7af31a2eb8e1e86a309bf5dbb79f3ef146f26
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-04-05 14:24:54 -0400

    Added more keywords to Question 59.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question59.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question59.m
@@ -17,7 +17,7 @@
   self.title = @"XOR decryption";
   self.answer = @"107359";
   self.number = @"59";
-  self.keywords = @"xor,decryption,god,key,ascii,values,encryption,password,import,unique,code,character,bytes,preferred,message,common,english,words,text";
+  self.keywords = @"xor,decryption,god,key,ascii,values,encryption,password,import,unique,code,character,bytes,preferred,message,common,english,words,text,containing,cyclically";
   self.estimatedComputationTime = @"0.139e-02";
   self.estimatedBruteForceComputationTime = @"0.127";
   
