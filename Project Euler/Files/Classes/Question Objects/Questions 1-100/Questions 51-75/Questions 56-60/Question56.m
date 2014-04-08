7d53527316e2ad5039fb2d007e6a486715a59748
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-04-08 08:27:40 -0400

    Added more keywords to Question 56.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question56.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question56.m
@@ -18,7 +18,7 @@
   self.title = @"Powerful digit sum";
   self.answer = @"972";
   self.number = @"56";
-  self.keywords = @"powers,powerful,digital,sum,maximum,massive,natural,numbers,googol";
+  self.keywords = @"powers,powerful,digital,sum,maximum,massive,natural,numbers,googol,considering,form";
   self.estimatedComputationTime = @"0.858";
   self.estimatedBruteForceComputationTime = @"9.41";
 }
