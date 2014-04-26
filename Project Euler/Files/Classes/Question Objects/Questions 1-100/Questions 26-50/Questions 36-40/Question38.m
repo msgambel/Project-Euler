75eeea87568771bcf23342bd248faa4bfb7f7476
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-04-26 16:21:25 -0400

    Added more keywords to Question 38. Also fixed a typo in the title.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question38.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question38.m
@@ -14,10 +14,10 @@
   
   self.date = @"28 February 2003";
   self.text = @"Take the number 192 and multiply it by each of 1, 2, and 3:\n\n192 x 1 = 192\n192 x 2 = 384\n192 x 3 = 576\n\nBy concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product of 192 and (1,2,3)\n\nThe same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).\n\nWhat is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?";
-  self.title = @"Pandigital mutiples";
+  self.title = @"Pandigital multiples";
   self.answer = @"932718654";
   self.number = @"38";
-  self.keywords = @"pandigital,multiples,digit,concatenated,product,integer,formed,largest,multiply";
+  self.keywords = @"pandigital,multiples,digit,concatenated,product,integer,formed,largest,multiply,lexographic,number";
   self.estimatedComputationTime = @"1.91e-04";
   self.estimatedBruteForceComputationTime = @"1.76e-03";
 }
