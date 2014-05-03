a41ca2349f57eda91f11bde63ee78f83ad72237d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-05-03 16:59:30 -0400

    Added more keywords to Question 31.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question31.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question31.m
@@ -25,7 +25,7 @@
   self.title = @"Coin sums";
   self.answer = @"73682";
   self.number = @"31";
-  self.keywords = @"pound,coins,currency,different,unique,ways,two,2,number,combinations,England,pence,circulation";
+  self.keywords = @"pounds,coins,sums,currency,different,unique,ways,two,2,number,combinations,England,pence,general,circulation";
   self.estimatedComputationTime = @"1.54e-04";
   self.estimatedBruteForceComputationTime = @"1.54e-04";
 }
