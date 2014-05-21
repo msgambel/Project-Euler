3158108fe41eca1d2180dbc159a8a42068653365
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-05-21 12:10:18 -0400

    Added more keywords to Question 13.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question13.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question13.m
@@ -24,7 +24,7 @@
   self.title = @"Large sum";
   self.answer = @"5537376230";
   self.number = @"13";
-  self.keywords = @"sum,big,int,large,first,digits,10,ten,150,one,hundred,and,fifty,numbers,list";
+  self.keywords = @"sum,big,int,large,first,digits,10,ten,150,one,hundred,and,fifty,numbers,list,work,out";
   self.estimatedComputationTime = @"2.86e-03";
   self.estimatedBruteForceComputationTime = @"3.28e-02";
 }
