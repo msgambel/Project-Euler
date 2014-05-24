ab52d318588517b3035aeab882b6720216473316
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-05-24 14:48:05 -0400

    Added more keywords to Question 10.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question10.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question10.m
@@ -17,7 +17,7 @@
   self.title = @"Summation of primes";
   self.answer = @"142913828922";
   self.number = @"10";
-  self.keywords = @"primes,summation,below,less,than,two,million,2000000,find,of,array";
+  self.keywords = @"primes,summation,below,less,than,two,million,2000000,find,of,array,long,int";
   self.estimatedComputationTime = @"1.4";
   self.estimatedBruteForceComputationTime = @"1.4";
 }
