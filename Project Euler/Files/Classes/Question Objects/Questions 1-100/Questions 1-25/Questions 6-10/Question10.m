70f75a174bc36897ef0a8a1ba243845bde0dd613
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-06-22 04:23:15 -0400

    Added a Hint to Question 10.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question10.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question10.m
@@ -13,6 +13,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"08 February 2002";
+  self.hint = @"Sieve of \"insert name here\". (long long int)'s should suffice for the sum.";
   self.text = @"The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.\n\nFind the sum of all the primes below two million.";
   self.title = @"Summation of primes";
   self.answer = @"142913828922";
