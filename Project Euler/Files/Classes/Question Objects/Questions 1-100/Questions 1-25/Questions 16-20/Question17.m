59efce90a9fd79930f18fe68a88f77ad63c43ab6
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-06-29 15:05:57 -0400

    Added a Hint to Question 17.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question17.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question17.m
@@ -21,6 +21,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"17 May 2002";
+  self.hint = @"Store how many letters are said in each of the hundreds, teens, tens, and units digits, and sum them all for every number.";
   self.text = @"If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.\n\nIf all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?\n\nNOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of \"and\" when writing out numbers is in compliance with British usage.";
   self.title = @"Number letter counts";
   self.answer = @"21124";
