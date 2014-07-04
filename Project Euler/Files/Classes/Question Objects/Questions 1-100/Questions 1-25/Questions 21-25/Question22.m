f0ccf7918c29e9699674e3b7963b7769069bfef5
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-07-04 15:01:07 -0400

    Added a Hint to Question 22.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question22.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question22.m
@@ -13,6 +13,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"19 July 2002";
+  self.hint = @"Make a helper method that takes a string and returns it's score.";
   self.text = @"Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.\n\nFor example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 x 53 = 49714.\n\nWhat is the total of all the name scores in the file?";
   self.title = @"Names scores";
   self.answer = @"871198282";
