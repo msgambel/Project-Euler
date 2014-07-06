0975bda1864ebdf394fc11d21c997d0a52a8564f
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-07-06 17:09:20 -0400

    Added a Hint to Question 24.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question24.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question24.m
@@ -13,6 +13,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"16 August 2002";
+  self.hint = @"Count backwards from the total number of 9-lexographic numbers.";
   self.text = @"A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:\n\n012   021   102   120   201   210\n\nWhat is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?";
   self.title = @"Lexicographic permutations";
   self.answer = @"2783915460";
