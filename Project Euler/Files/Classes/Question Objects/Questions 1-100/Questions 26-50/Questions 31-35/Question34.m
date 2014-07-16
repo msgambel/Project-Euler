b192baf1f7fa9f2a8e922e70dc80ad7f59fe7370
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-07-16 13:35:56 -0400

    Added a Hint to Question 34.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question34.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question34.m
@@ -13,6 +13,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"03 January 2003";
+  self.hint = @"You only have to check up to 50,000 (consider a number with only 8's in it).";
   self.text = @"145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.\n\nFind the sum of all numbers which are equal to the sum of the factorial of their digits.\n\nNote: as 1! = 1 and 2! = 2 are not sums they are not included.";
   self.title = @"Digit factorials";
   self.answer = @"40730";
