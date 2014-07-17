1efdafff0ad85f0dbc5440116dc54c4142b0ad9b
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-07-17 16:13:21 -0400

    Added a Hint to Question 35.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question35.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question35.m
@@ -20,6 +20,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"17 January 2003";
+  self.hint = @"The only digits a circular prime could have are 1, 3, 7, and 9.";
   self.text = @"The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.\n\nThere are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.\n\nHow many circular primes are there below one million?";
   self.title = @"Circular primes";
   self.answer = @"55";
