e545a6655c63e9701a6e779eea0d2d7243b569e2
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-07-25 03:03:09 -0400

    Added a Hint to Question 43.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question43.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question43.m
@@ -19,6 +19,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"09 May 2003";
+  self.hint = @"Switch between logic and brute force (d6 = 5).";
   self.text = @"The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.\n\nLet d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:\n\nd2d3d4=406 is divisible by 2\nd3d4d5=063 is divisible by 3\nd4d5d6=635 is divisible by 5\nd5d6d7=357 is divisible by 7\nd6d7d8=572 is divisible by 11\nd7d8d9=728 is divisible by 13\nd8d9d10=289 is divisible by 17\n\nFind the sum of all 0 to 9 pandigital numbers with this property.";
   self.title = @"Sub-string divisibility";
   self.answer = @"16695334890";
