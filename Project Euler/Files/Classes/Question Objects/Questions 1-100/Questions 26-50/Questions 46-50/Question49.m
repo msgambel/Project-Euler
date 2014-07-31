ae20e37ed2636bb6e0cb413a94438fc02300731f
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-07-31 13:53:36 -0400

    Added a Hint to Question 49.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question49.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question49.m
@@ -13,6 +13,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"01 August 2003";
+  self.hint = @"Split the prime numbers between 1,000 and 10,000 into 'small', 'medium', and 'large' numbers.";
   self.text = @"The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways: (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.\n\nThere are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property, but there is one other 4-digit increasing sequence.\n\nWhat 12-digit number do you form by concatenating the three terms in this sequence?";
   self.title = @"Prime permutations";
   self.answer = @"296962999629";
