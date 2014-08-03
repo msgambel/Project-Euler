c958fa467dbd81aae8d6e86dde82185d38bfbe85
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-08-03 16:31:34 -0400

    Added a Hint to Question 52.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question52.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question52.m
@@ -13,6 +13,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"12 September 2003";
+  self.hint = @"It's a (low) 6 digit number.";
   self.text = @"It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.\n\nFind the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.";
   self.title = @"Permuted multiples";
   self.answer = @"142857";
