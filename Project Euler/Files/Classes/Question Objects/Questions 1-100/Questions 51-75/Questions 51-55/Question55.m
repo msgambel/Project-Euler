9eb24b1ee51fcc263c96634d498ddfd4b11b5af8
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-08-06 15:58:38 -0400

    Added a Hint to Question 55. Also fixed a typo in the comments.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question55.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question55.m
@@ -14,6 +14,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"24 October 2003";
+  self.hint = @"Store the previous Lychrel numbers as you find them to compare against later sequences.";
   self.text = @"If we take 47, reverse and add, 47 + 74 = 121, which is palindromic.\n\nNot all numbers produce palindromes so quickly. For example,\n\n349 + 943 = 1292,\n1292 + 2921 = 4213\n4213 + 3124 = 7337\n\nThat is, 349 took three iterations to arrive at a palindrome.\n\nAlthough no one has proved it yet, it is thought that some numbers, like 196, never produce a palindrome. A number that never forms a palindrome through the reverse and add process is called a Lychrel number. Due to the theoretical nature of these numbers, and for the purpose of this problem, we shall assume that a number is Lychrel until proven otherwise. In addition you are given that for every number below ten-thousand, it will either (i) become a palindrome in less than fifty iterations, or, (ii) no one, with all the computing power that exists, has managed so far to map it to a palindrome. In fact, 10677 is the first number to be shown to require over fifty iterations before producing a palindrome: 4668731596684224866951378664 (53 iterations, 28-digits).\n\nSurprisingly, there are palindromic numbers that are themselves Lychrel numbers; the first example is 4994.\n\nHow many Lychrel numbers are there below ten-thousand?\n\nNOTE: Wording was modified slightly on 24 April 2007 to emphasise the theoretical nature of Lychrel numbers.";
   self.title = @"Lychrel numbers";
   self.answer = @"249";
@@ -34,7 +35,7 @@
   
   // Here, we simply take the number, reverse it, add them together and check if
   // the sum is a Lychrel number or not. We start by assuming that all the
-  // numbers from 1 up to 10,000 are Lychrel. If thre number is NOT a Lychrel
+  // numbers from 1 up to 10,000 are Lychrel. If the number is NOT a Lychrel
   // number, we decrement the number os Lychrel numbers by 1.
   //
   // In order to get some speed gains, we only use the BigInt implementation
@@ -170,7 +171,7 @@
   
   // Here, we simply take the number, reverse it, add them together and check if
   // the sum is a Lychrel number or not. We start by assuming that all the
-  // numbers from 1 up to 10,000 are Lychrel. If thre number is NOT a Lychrel
+  // numbers from 1 up to 10,000 are Lychrel. If the number is NOT a Lychrel
   // number, we decrement the number os Lychrel numbers by 1.
   
   // Veriable to hold the maximum Lychrel number to check.
