139bf0a64af8a3583eef1d0098f12c7a151d5709
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-08-14 03:56:07 -0400

    Added a Hint to Question 63.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
@@ -13,6 +13,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"13 February 2004";
+  self.hint = @"The log function is your friend!";
   self.text = @"The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the 9-digit number, 134217728=8^9, is a ninth power.\n\nHow many n-digit positive integers exist which are also an nth power?";
   self.title = @"Powerful digit counts";
   self.answer = @"49";
