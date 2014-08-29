36d6a9ef6f600cf116ac06696c1b63c3c8eca424
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-08-29 03:05:58 -0400

    Added a Hint to Question 78.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question78.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question78.m
@@ -14,6 +14,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"10 September 2004";
+  self.hint = @"Use the Pentagonal number theorem.";
   self.text = @"Let p(n) represent the number of different ways in which n coins can be separated into piles. For example, five coins can separated into piles in exactly seven different ways, so p(5)=7.\n\nOOOOO\nOOOO   O\nOOO   OO\nOOO   O   O\nOO   OO   O\nOO   O   O   O\nO   O   O   O   O\n\nFind the least value of n for which p(n) is divisible by one million.";
   self.title = @"Coin partitions";
   self.answer = @"55374";
