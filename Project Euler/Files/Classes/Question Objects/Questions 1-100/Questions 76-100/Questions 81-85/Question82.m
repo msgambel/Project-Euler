440fce577ae37a501505aba422e8b5bb181f1b3e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-09-02 07:26:59 -0400

    Added a Hint to Question 82.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question82.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question82.m
@@ -13,6 +13,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"05 November 2004";
+  self.hint = @"Store the values of each node in the matrix, and iterate backwards.";
   self.text = @"NOTE: This problem is a more challenging version of Problem 81.\n\nThe minimal path sum in the 5 by 5 matrix below, by starting in any cell in the left column and finishing in any cell in the right column, and only moving up, down, and right, is indicated in red and bold; the sum is equal to 994.\n\n131	673	234	103	18\n201	96	342	965	150\n630	803	746	422	111\n537	699	497	121	956\n805	732	524	37	331\n\nFind the minimal path sum, in matrix.txt (right click and 'Save Link/Target As...'), a 31K text file containing a 80 by 80 matrix, from the left column to the right column.";
   self.title = @"Path sum: three ways";
   self.answer = @"260324";
