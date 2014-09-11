e5a45e147e2c450706d01719623cfc0e988a3c34
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-09-11 13:47:39 -0400

    Added a Hint to Question 91.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question91.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question91.m
@@ -13,6 +13,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"18 March 2005";
+  self.hint = @"The inner product of two orthogonal vectors is 0.";
   self.text = @"The points P (x1, y1) and Q (x2, y2) are plotted at integer co-ordinates and are joined to the origin, O(0,0), to form ΔOPQ.\n\nMissing Image!!!\n\nThere are exactly fourteen triangles containing a right angle that can be formed when each co-ordinate lies between 0 and 2 inclusive; that is,\n\n0 <= x1, y1, x2, y2 <= 2.\n\nMissing Images!!!\n\nGiven that 0 <= x1, y1, x2, y2 <= 50, how many right triangles can be formed?";
   self.title = @"Right triangles with integer coordinates";
   self.answer = @"14234";
