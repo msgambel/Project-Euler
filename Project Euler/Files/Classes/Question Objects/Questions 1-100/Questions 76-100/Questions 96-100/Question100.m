2e12ece169660be6210b0c814a4442b4a008792a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-09-20 14:59:49 -0400

    Added a Hint to Question 100.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 96-100/Question100.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 96-100/Question100.m
@@ -13,6 +13,7 @@
   // the brute force method and the optimized way to solve the problem.
   
   self.date = @"15 July 2005";
+  self.hint = @"Use some algebra and the quadratic formula!";
   self.text = @"If a box contains twenty-one coloured discs, composed of fifteen blue discs and six red discs, and two discs were taken at random, it can be seen that the probability of taking two blue discs, P(BB) = (15/21)(14/20) = 1/2.\n\nThe next such arrangement, for which there is exactly 50% chance of taking two blue discs at random, is a box containing eighty-five blue discs and thirty-five red discs.\n\nBy finding the first arrangement to contain over 10^12 = 1,000,000,000,000 discs in total, determine the number of blue discs that the box would contain.";
   self.title = @"Arranged probability";
   self.answer = @"756872327473";
