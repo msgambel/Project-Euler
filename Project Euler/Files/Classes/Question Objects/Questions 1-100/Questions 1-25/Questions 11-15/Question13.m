28d33fdd4268e8554da70fd7c46e9bed142caaa1
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-10-15 14:11:00 -0400

    Added a Rating to Question 13.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question13.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question13.m
@@ -25,6 +25,7 @@
   self.title = @"Large sum";
   self.answer = @"5537376230";
   self.number = @"13";
+  self.rating = @"4";
   self.keywords = @"sum,big,int,large,first,digits,10,ten,150,one,hundred,and,fifty,numbers,list,work,out";
   self.estimatedComputationTime = @"2.86e-03";
   self.estimatedBruteForceComputationTime = @"3.28e-02";
