4d13db57215a6b4f4c8ca79c5cc55d69bef7951f
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-10-29 15:12:22 -0400

    Added a Rating to Question 27.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question27.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question27.m
@@ -25,6 +25,7 @@
   self.title = @"Quadratic primes";
   self.answer = @"-59231";
   self.number = @"27";
+  self.rating = @"5";
   self.keywords = @"consecutive,odd,primes,formula,coefficients,quadratic,maximum,expression,production,absolute,value,produces,number,helper,methods";
   self.estimatedComputationTime = @"0.23";
   self.estimatedBruteForceComputationTime = @"1.44";
