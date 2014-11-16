28df23c56d4d01d0a84a2b709d53b4801e581bf3
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-11-16 14:31:17 -0500

    Added a Rating to Question 45.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question45.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question45.m
@@ -18,6 +18,7 @@
   self.title = @"Pandigital mutiples";
   self.answer = @"1533776805";
   self.number = @"45";
+  self.rating = @"4";
   self.keywords = @"pandigital,multiples,triangle,pentagonal,hexagonal,concatenated,product,9,nine,digit,number,formed,integer,odd,largest";
   self.estimatedComputationTime = @"8.31e-04";
   self.estimatedBruteForceComputationTime = @"8.31e-04";
