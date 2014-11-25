4642223f907da6457777b857f258f69bd0ac0d67
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-11-25 05:24:31 -0500

    Added a Rating to Question 54.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question54.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question54.m
@@ -19,6 +19,7 @@
   self.title = @"Poker hands";
   self.answer = @"376";
   self.number = @"54";
+  self.rating = @"5";
   self.keywords = @"poker,hands,two,player,import,rank,compare,5,five,clear,winner,specific,order";
   self.estimatedComputationTime = @"3.43e-02";
   self.estimatedBruteForceComputationTime = @"3.43e-02";
