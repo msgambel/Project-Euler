9def94ed8c5ba02691a973ecddc6c7398d910146
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-12-04 15:26:16 -0500

    Added a Rating to Question 63.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
@@ -18,6 +18,7 @@
   self.title = @"Powerful digit counts";
   self.answer = @"49";
   self.number = @"63";
+  self.rating = @"4";
   self.keywords = @"powers,digits,count,fifth,nine,9,exist,positive,integers,number,nth,bases,exponents";
   self.estimatedComputationTime = @"2.8e-05";
   self.estimatedBruteForceComputationTime = @"2.8e-05";
