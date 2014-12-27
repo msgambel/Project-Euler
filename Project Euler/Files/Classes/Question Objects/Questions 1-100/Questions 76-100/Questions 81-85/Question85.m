1269e0f74226b94926906d9989fd8638e017b9e4
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-12-26 23:08:10 -0500

    Added a Rating to Question 85.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question85.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question85.m
@@ -19,6 +19,7 @@
   self.title = @"Counting rectangles";
   self.answer = @"2772";
   self.number = @"85";
+  self.rating = @"5";
   self.keywords = @"rectangles,grid,size,counting,triangle,numbers,2000000,two,million,nearest,solution,area,sub";
   self.estimatedComputationTime = @"2.13e-04";
   self.estimatedBruteForceComputationTime = @"2.13e-04";
