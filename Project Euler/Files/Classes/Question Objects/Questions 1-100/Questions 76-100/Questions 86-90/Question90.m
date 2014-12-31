fecece550b3f75f3a9d7efc931f04bdd457e3d04
Michael Sgambelluri <michaelsgam10@hotmail.com>
2014-12-31 03:15:04 -0500

    Added a Rating to Question 90.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question90.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question90.m
@@ -24,6 +24,7 @@
   self.title = @"Cube digit pairs";
   self.answer = @"1217";
   self.number = @"90";
+  self.rating = @"4";
   self.keywords = @"cube,digit,pairs,distinct,formations,arrangements,square,numbers,different,positions,side,6,six,2,two,displayed,forming";
   self.estimatedComputationTime = @"0.173";
   self.estimatedBruteForceComputationTime = @"0.173";
