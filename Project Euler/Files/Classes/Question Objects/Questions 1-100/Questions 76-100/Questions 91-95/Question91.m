00cad06e25cd731951a4c180f649f6a96f993405
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-01-01 13:51:47 -0500

    Added a Rating to Question 91.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question91.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question91.m
@@ -18,6 +18,7 @@
   self.title = @"Right triangles with integer coordinates";
   self.answer = @"14234";
   self.number = @"91";
+  self.rating = @"4";
   self.keywords = @"right,triangles,with,integer,coordinates,vectors,inner,product,orthogonal,points,formed";
   self.estimatedComputationTime = @"0.105";
   self.estimatedBruteForceComputationTime = @"0.105";
