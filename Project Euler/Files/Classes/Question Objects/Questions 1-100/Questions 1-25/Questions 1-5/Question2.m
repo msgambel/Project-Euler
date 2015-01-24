85bf57cce85571dfb786273cdbf7cb91970b44f2
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-01-24 04:36:03 -0500

    Added a Difficulty to Question 2.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question2.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question2.m
@@ -20,6 +20,7 @@
   self.number = @"2";
   self.rating = @"4";
   self.keywords = @"fibonacci,sequence,numbers,sum,even,valued,terms,exceed,four,million,4000000,odd,positive,generated,previous";
+  self.difficulty = @"Meh";
   self.estimatedComputationTime = @"2.1e-05";
   self.estimatedBruteForceComputationTime = @"4.7e-05";
 }
