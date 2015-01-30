345e3496026192ca053f7db97f04889e97bfff64
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-01-30 03:50:18 -0500

    Added a Difficulty to Question 8.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question8.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question8.m
@@ -27,6 +27,7 @@
   self.number = @"8";
   self.rating = @"4";
   self.keywords = @"largest,product,in,a,series,five,5,consecutive,digits,1000,one,thousand,greatest,sub,string,number,search";
+  self.difficulty = @"Meh";
   self.estimatedComputationTime = @"6.34e-04";
   self.estimatedBruteForceComputationTime = @"2.72e-03";
 }
