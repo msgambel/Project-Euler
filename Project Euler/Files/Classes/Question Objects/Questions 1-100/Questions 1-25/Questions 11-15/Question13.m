f64c8c5b5328e070c102be41701368ac1faf5771
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-02-04 10:17:35 -0500

    Added a Difficulty to Question 13.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question13.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question13.m
@@ -27,6 +27,7 @@
   self.number = @"13";
   self.rating = @"4";
   self.keywords = @"sum,big,int,large,first,digits,10,ten,150,one,hundred,and,fifty,numbers,list,work,out";
+  self.difficulty = @"Meh";
   self.estimatedComputationTime = @"2.86e-03";
   self.estimatedBruteForceComputationTime = @"3.28e-02";
 }
