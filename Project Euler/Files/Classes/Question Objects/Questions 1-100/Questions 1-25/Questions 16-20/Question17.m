75027812e2d1ff8ce295864d95a4d5ac7a649e78
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-02-08 18:40:46 -0500

    Added a Difficulty to Question 17.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question17.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question17.m
@@ -28,6 +28,7 @@
   self.number = @"17";
   self.rating = @"4";
   self.keywords = @"letters,sum,words,numbers,counts,british,usage,written,one,thousand,1000,contains,spaces,hyphens,compliance";
+  self.difficulty = @"Meh";
   self.estimatedComputationTime = @"2.5e-05";
   self.estimatedBruteForceComputationTime = @"5.2e-05";
 }
