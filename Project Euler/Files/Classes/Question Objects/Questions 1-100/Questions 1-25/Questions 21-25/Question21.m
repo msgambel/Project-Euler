155b9761eaee5d9f9eb2591af53fdf596d15d6af
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-02-12 19:33:02 -0500

    Added a Difficulty to Question 21.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question21.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question21.m
@@ -21,6 +21,7 @@
   self.number = @"21";
   self.rating = @"5";
   self.keywords = @"divisors,proper,amicable,pair,numbers,sum,10000,ten,thousand,evalute,under,prime,factorization";
+  self.difficulty = @"Easy";
   self.estimatedComputationTime = @"1.71e-02";
   self.estimatedBruteForceComputationTime = @"9.36e-02";
 }
