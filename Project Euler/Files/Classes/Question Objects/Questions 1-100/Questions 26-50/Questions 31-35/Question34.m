524307d71ff8d8a0f236616c32114c5096fac27f
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-02-25 06:33:47 -0500

    Added a Difficulty to Question 34.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question34.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question34.m
@@ -20,6 +20,7 @@
   self.number = @"34";
   self.rating = @"5";
   self.keywords = @"digit,sum,factorials,equal,curious,numbers,fifty,thousand,50000,two,2";
+  self.difficulty = @"Easy";
   self.estimatedComputationTime = @"7.71e-03";
   self.estimatedBruteForceComputationTime = @"7.71e-03";
 }
