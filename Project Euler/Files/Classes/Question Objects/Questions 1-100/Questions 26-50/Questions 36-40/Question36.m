249af6927d3a9a189ce3351a5616818c4f2ba2af
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-02-27 04:46:15 -0500

    Added a Difficulty to Question 36.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question36.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question36.m
@@ -26,6 +26,7 @@
   self.number = @"36";
   self.rating = @"5";
   self.keywords = @"Double-base,double,base,palindromes,palindromic,2,two,10,ten,decimal,number,one,million,1000000,less,leading,zeros";
+  self.difficulty = @"Easy";
   self.estimatedComputationTime = @"7.61e-02";
   self.estimatedBruteForceComputationTime = @"6.11";
 }
