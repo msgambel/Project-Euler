98f458ab33194508f238e511b7e0e0bb19396005
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-02-28 17:16:39 -0500

    Added a Difficulty to Question 37.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question37.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question37.m
@@ -29,6 +29,7 @@
   self.number = @"37";
   self.rating = @"5";
   self.keywords = @"primes,left,right,truncatable,continuously,remove,digits,7,seven,sum,only,hash,table";
+  self.difficulty = @"Medium";
   self.estimatedComputationTime = @"0.99";
   self.estimatedBruteForceComputationTime = @"1.14";
 }
