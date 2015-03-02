cb1418700158bdbe3b254b44b05f5e6357a33ca9
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-03-02 04:44:48 -0500

    Added a Difficulty to Question 39.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question39.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question39.m
@@ -20,6 +20,7 @@
   self.number = @"39";
   self.rating = @"4";
   self.keywords = @"pythagorean,triple,perimeter,right,angle,length,sides,maximized,integral,1000,one,thousand,solutions,integer,triangles,pair,maximum";
+  self.difficulty = @"Easy";
   self.estimatedComputationTime = @"2.43e-03";
   self.estimatedBruteForceComputationTime = @"2.43e-03";
 }
