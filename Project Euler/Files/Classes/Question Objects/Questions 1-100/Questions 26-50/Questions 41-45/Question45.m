827c99e937db43c25c2f0bcf94f58a77efc09f31
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-03-08 14:20:26 -0400

    Added a Difficulty to Question 45.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question45.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question45.m
@@ -20,6 +20,7 @@
   self.number = @"45";
   self.rating = @"4";
   self.keywords = @"pandigital,multiples,triangle,pentagonal,hexagonal,concatenated,product,9,nine,digit,number,formed,integer,odd,largest";
+  self.difficulty = @"Easy";
   self.estimatedComputationTime = @"8.31e-04";
   self.estimatedBruteForceComputationTime = @"8.31e-04";
 }
