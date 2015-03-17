2b3a45d276ede92a78c5fad1168be0bd5234db58
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-03-17 13:18:20 -0400

    Added a Difficulty to Question 54.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question54.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question54.m
@@ -21,6 +21,7 @@
   self.number = @"54";
   self.rating = @"5";
   self.keywords = @"poker,hands,two,player,import,rank,compare,5,five,clear,winner,specific,order";
+  self.difficulty = @"Easy";
   self.estimatedComputationTime = @"3.43e-02";
   self.estimatedBruteForceComputationTime = @"3.43e-02";
 }
