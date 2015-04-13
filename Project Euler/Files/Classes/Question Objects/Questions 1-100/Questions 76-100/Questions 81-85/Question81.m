c16e374d5c74502681d5381a6cfdf52b617c10f0
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-04-13 15:30:01 -0400

    Added a Difficulty to Question 81.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question81.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question81.m
@@ -21,6 +21,7 @@
   self.number = @"81";
   self.rating = @"4";
   self.keywords = @"matrix,a*,a,star,minimal,path,sum,import,80,eighty,2,two,ways,files,matrix";
+  self.difficulty = @"Easy";
   self.estimatedComputationTime = @"2.73e-02";
   self.estimatedBruteForceComputationTime = @"2.73e-02";
 }
