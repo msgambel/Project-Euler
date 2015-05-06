682cfb950cf03d9db6c6c2588961b1789b330a71
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-05-06 09:24:11 -0400

    Added a Difficulty to Question 131.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 126-150/Questions 131-135/Question131.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 126-150/Questions 131-135/Question131.m
@@ -20,6 +20,7 @@
   self.number = @"131";
   self.rating = @"5";
   self.keywords = @"primes,cube,partnership,factoring,positive,integer,property,difference,of,cubes,one,million,1000000,value,unique";
+  self.difficulty = @"Easy";
   self.estimatedComputationTime = @"1.77e-04";
   self.estimatedBruteForceComputationTime = @"1.77e-04";
 }
