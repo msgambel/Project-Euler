b180b78ec3032859bec5c4f4cd0a1903eeeec350
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-06-07 16:23:08 -0400

    Added a isFun to Question 22.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question22.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question22.m
@@ -15,6 +15,7 @@
   self.date = @"19 July 2002";
   self.hint = @"Make a helper method that takes a string and returns it's score.";
   self.text = @"Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.\n\nFor example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 x 53 = 49714.\n\nWhat is the total of all the name scores in the file?";
+  self.isFun = YES;
   self.title = @"Names scores";
   self.answer = @"871198282";
   self.number = @"22";
