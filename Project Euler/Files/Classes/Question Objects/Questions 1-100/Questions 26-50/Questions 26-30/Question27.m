442f782269e83b59c14cdb9ba75a5c9ddeacc47a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-06-12 13:34:53 -0400

    Added a isFun to Question 27.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question27.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question27.m
@@ -22,6 +22,7 @@
   self.date = @"27 September 2002";
   self.hint = @"The coefficient a must be odd, and b must be a prime.";
   self.text = @"Euler published the remarkable quadratic formula:\n\nn² + n + 41\n\nIt turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39. However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.\n\nUsing computers, the incredible formula  n²  79n + 1601 was discovered, which produces 80 primes for the consecutive values n = 0 to 79. The product of the coefficients, 79 and 1601, is 126479.\n\nConsidering quadratics of the form:\n\nn² + an + b, where |a| < 1000 and |b| < 1000\n\nwhere |n| is the modulus/absolute value of n\ne.g. |11| = 11 and |-4| = 4\n\nFind the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n = 0.";
+  self.isFun = YES;
   self.title = @"Quadratic primes";
   self.answer = @"-59231";
   self.number = @"27";
