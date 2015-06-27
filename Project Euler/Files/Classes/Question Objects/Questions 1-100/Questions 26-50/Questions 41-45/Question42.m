3ad44e87b554a9f8a5bb2172caf11c7019927223
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-06-27 15:53:21 -0400

    Added a isFun to Question 42.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question42.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question42.m
@@ -16,6 +16,7 @@
   self.date = @"25 April 2003";
   self.hint = @"Figure out the maximum triangle number by computing the length of the longest word in the words.txt file, and multiplying it by 26 (the max value a letter can have).";
   self.text = @"The nth term of the sequence of triangle numbers is given by, t(n) = ½n(n+1); so the first ten triangle numbers are:\n\n1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...\n\nBy converting each letter in a word to a number corresponding to its alphabetical position and adding these values we form a word value. For example, the word value for SKY is 19 + 11 + 25 = 55 = t(10). If the word value is a triangle number then we shall call the word a triangle word.\n\nUsing words.txt (right click and 'Save Link/Target As...'), a 16K text file containing nearly two-thousand common English words, how many are triangle words?";
+  self.isFun = YES;
   self.title = @"Coded triangle numbers";
   self.answer = @"162";
   self.number = @"42";
