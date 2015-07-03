6d538d396169cdfaa1afbc116960a7478b8b25ca
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-07-03 15:38:19 -0400

    Added a isFun to Question 48.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question48.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question48.m
@@ -16,6 +16,7 @@
   self.date = @"18 July 2003";
   self.hint = @"Use Modular Exponentiation.";
   self.text = @"The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.\n\nFind the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.";
+  self.isFun = NO;
   self.title = @"Self powers";
   self.answer = @"9110846700";
   self.number = @"48";
