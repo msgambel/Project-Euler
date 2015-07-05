c7df15bcb3f501868cdb5ca7543cea53cb960c3f
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-07-05 04:31:03 -0400

    Added a isFun to Question 50.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question50.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question50.m
@@ -15,6 +15,7 @@
   self.date = @"15 August 2003";
   self.hint = @"Use a floating sum of consecutive primes (subtract the smallest, add the next largest).";
   self.text = @"The prime 41, can be written as the sum of six consecutive primes:\n\n41 = 2 + 3 + 5 + 7 + 11 + 13\n\nThis is the longest sum of consecutive primes that adds to a prime below one-hundred.\n\nThe longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.\n\nWhich prime, below one-million, can be written as the sum of the most consecutive primes?";
+  self.isFun = YES;
   self.title = @"Consecutive prime sum";
   self.answer = @"997651";
   self.number = @"50";
