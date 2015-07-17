4dcc126c983d41a99a92976f39cfab26cca555d5
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-07-17 15:29:48 -0400

    Added a isFun to Question 62.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question62.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question62.m
@@ -15,6 +15,7 @@
   self.date = @"30 January 2004";
   self.hint = @"Compute all the cubes with the same number of digits first.";
   self.text = @"The cube, 41063625 (345³), can be permuted to produce two other cubes: 56623104 (384³) and 66430125 (405³). In fact, 41063625 is the smallest cube which has exactly three permutations of its digits which are also cube.\n\nFind the smallest cube for which exactly five permutations of its digits are cube.";
+  self.isFun = YES;
   self.title = @"Cubic permutations";
   self.answer = @"127035954683";
   self.number = @"62";
