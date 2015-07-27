d1ab27079cd82c9763ae13ab2e8bae929081820f
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-07-27 04:23:45 -0400

    Added a isFun to Question 72.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question72.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question72.m
@@ -16,6 +16,7 @@
   self.date = @"18 June 2004";
   self.hint = @"Use the Euler Totient Function to generate all of the numbers that are relatively prime to a given number.";
   self.text = @"Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a reduced proper fraction.\n\nIf we list the set of reduced proper fractions for d <= 8 in ascending order of size, we get:\n\n1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8\n\nIt can be seen that there are 21 elements in this set.\n\nHow many elements would be contained in the set of reduced proper fractions for d <= 1,000,000?";
+  self.isFun = YES;
   self.title = @"Counting fractions";
   self.answer = @"303963552391";
   self.number = @"72";
