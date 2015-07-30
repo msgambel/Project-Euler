718509a7af4ab338fe23b2e7503169cf388fc58d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-07-30 05:52:25 -0400

    Added a isFun to Question 75.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question75.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question75.m
@@ -15,6 +15,7 @@
   self.date = @"30 July 2004";
   self.hint = @"There's a formula to generate all the pythagorean triplets!";
   self.text = @"It turns out that 12 cm is the smallest length of wire that can be bent to form an integer sided right angle triangle in exactly one way, but there are many more examples.\n\n12 cm: (3,4,5)\n24 cm: (6,8,10)\n30 cm: (5,12,13)\n36 cm: (9,12,15)\n40 cm: (8,15,17)\n48 cm: (12,16,20)\n\nIn contrast, some lengths of wire, like 20 cm, cannot be bent to form an integer sided right angle triangle, and other lengths allow more than one solution to be found; for example, using 120 cm it is possible to form exactly three different integer sided right angle triangles.\n\n120 cm: (30,40,50), (20,48,52), (24,45,51)\n\nGiven that L is the length of the wire, for how many values of L <= 1,500,000 can exactly one integer sided right angle triangle be formed?";
+  self.isFun = YES;
   self.title = @"Singular integer right trangles";
   self.answer = @"161667";
   self.number = @"75";
