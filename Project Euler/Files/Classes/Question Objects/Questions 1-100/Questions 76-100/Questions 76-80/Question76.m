dd44ed62fc6fb84468bd5a50ed65caa2af7f4a4a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-07-31 06:20:22 -0400

    Added a isFun to Question 76.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question76.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question76.m
@@ -21,6 +21,7 @@
   self.date = @"13 August 2004";
   self.hint = @"A recursive function would do well here.";
   self.text = @"It is possible to write five as a sum in exactly six different ways:\n\n4 + 1\n3 + 2\n3 + 1 + 1\n2 + 2 + 1\n2 + 1 + 1 + 1\n1 + 1 + 1 + 1 + 1\n\nHow many different ways can one hundred be written as a sum of at least two positive integers?";
+  self.isFun = YES;
   self.title = @"Counting summations";
   self.answer = @"190569291";
   self.number = @"76";
