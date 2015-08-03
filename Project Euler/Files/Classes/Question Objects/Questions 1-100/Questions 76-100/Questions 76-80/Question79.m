58c7b6f98d24d70bbd1f6d8971d86aabdad510e1
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-08-03 04:32:02 -0400

    Added a isFun to Question 79.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question79.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question79.m
@@ -21,6 +21,7 @@
   self.date = @"17 September 2004";
   self.hint = @"Assume the passcode has no duplicates.";
   self.text = @"A common security method used for online banking is to ask the user for three random characters from a passcode. For example, if the passcode was 531278, they may ask for the 2nd, 3rd, and 5th characters; the expected reply would be: 317.\n\nThe text file, keylog.txt, contains fifty successful login attempts.\n\nGiven that the three characters are always asked for in order, analyse the file so as to determine the shortest possible secret passcode of unknown length.";
+  self.isFun = YES;
   self.title = @"Passcode derivation";
   self.answer = @"73162890";
   self.number = @"79";
