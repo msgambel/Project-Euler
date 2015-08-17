b23828dec393dc1ba1700b84f1a821d0ece87858
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-08-17 03:50:52 -0400

    Added a isFun to Question 93.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question93.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question93.m
@@ -23,6 +23,7 @@
   self.date = @"15 April 2005";
   self.hint = @"Reverse Polish Notation should do the trick.";
   self.text = @"By using each of the digits from the set, {1, 2, 3, 4}, exactly once, and making use of the four arithmetic operations (+, -, *, /) and brackets/parentheses, it is possible to form different positive integer targets.\n\nFor example,\n\n8 = (4 * (1 + 3)) / 2\n14 = 4 * (3 + 1 / 2)\n19 = 4 * (2 + 3) - 1\n36 = 3 * 4 * (2 + 1)\n\nNote that concatenations of the digits, like 12 + 34, are not allowed.\n\nUsing the set, {1, 2, 3, 4}, it is possible to obtain thirty-one different target numbers of which 36 is the maximum, and each of the numbers 1 to 28 can be obtained before encountering the first non-expressible number.\n\nFind the set of four distinct digits, a < b < c < d, for which the longest set of consecutive positive integers, 1 to n, can be obtained, giving your answer as a string: abcd.";
+  self.isFun = NO;
   self.title = @"Arithmetic expressions";
   self.answer = @"1258";
   self.number = @"93";
