16dfc7cc5033bdd6a636eeec4e273ec20bd0216d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-08-26 12:15:47 -0400

    Added a isFun to Question 120.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
@@ -15,6 +15,7 @@
   self.date = @"21 April 2006";
   self.hint = @"Just use modular arithmetic to compute rmax.";
   self.text = @"Let r be the remainder when (a-1)^n + (a+1)^n is divided by a^2.\n\nFor example, if a = 7 and n = 3, then r = 42: 63 + 83 = 728 = 42 mod 49. And as n varies, so too will r, but for a = 7 it turns out that rmax = 42.\n\nFor 3 <= a <= 1000, find sum(rmax).";
+  self.isFun = YES;
   self.title = @"Square remainders";
   self.answer = @"333082500";
   self.number = @"120";
