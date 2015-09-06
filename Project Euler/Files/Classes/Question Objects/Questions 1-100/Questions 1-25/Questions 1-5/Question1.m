51bb630778c075635b9abc3a1bea4ad2101f2ab1
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-09-06 11:59:18 -0400

    Added a Solution Line Count to Question 1.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question1.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question1.m
@@ -28,6 +28,7 @@
   self.rating = @"5";
   self.keywords = @"sums,triangle,numbers,multiples,3,three,5,five,1000,one,thousand,first,problem,question,list";
   self.difficulty = @"Meh";
+  self.solutionLineCount = @"11";
   self.estimatedComputationTime = @"2.1e-05";
   self.estimatedBruteForceComputationTime = @"2.7e-05";
 }
