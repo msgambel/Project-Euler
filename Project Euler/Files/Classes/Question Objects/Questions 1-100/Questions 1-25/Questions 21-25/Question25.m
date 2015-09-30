01a3d8362e17fde5ad0718cd0254a0a9a13c6e29
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-09-30 05:07:37 -0400

    Added a Solution Line Count to Question 25.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question25.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question25.m
@@ -23,6 +23,7 @@
   self.rating = @"4";
   self.keywords = @"fibonacci,closed,form,recurrance,terms,sequence,digits,contains,1000,one,thousand,first,number,expression,logarithm";
   self.difficulty = @"Easy";
+  self.solutionLineCount = @"3";
   self.estimatedComputationTime = @"4.6e-05";
   self.estimatedBruteForceComputationTime = @"3.25e-02";
 }
