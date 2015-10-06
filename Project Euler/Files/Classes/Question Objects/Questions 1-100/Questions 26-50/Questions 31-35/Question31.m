8369715b2e5396773b597251ea331a94c2e5c599
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-10-06 14:49:27 -0400

    Added a Solution Line Count to Question 31.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question31.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question31.m
@@ -30,6 +30,7 @@
   self.rating = @"4";
   self.keywords = @"pounds,coins,sums,currency,different,unique,ways,two,2,number,combinations,England,pence,general,circulation";
   self.difficulty = @"Medium";
+  self.solutionLineCount = @"25";
   self.estimatedComputationTime = @"1.54e-04";
   self.estimatedBruteForceComputationTime = @"1.54e-04";
 }
