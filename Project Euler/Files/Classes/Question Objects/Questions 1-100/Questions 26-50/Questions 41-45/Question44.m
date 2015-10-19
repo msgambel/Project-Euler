743a08f2c450f60060ec85342381c20852a90e72
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-10-19 10:47:07 -0400

    Added a Solution Line Count to Question 44.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question44.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question44.m
@@ -22,6 +22,7 @@
   self.rating = @"5";
   self.keywords = @"pair,pentagonal,numbers,sum,difference,generated,formula,recursion,difference,quadratic,equations,factoring,radicals";
   self.difficulty = @"Easy";
+  self.solutionLineCount = @"29";
   self.estimatedComputationTime = @"7.91e-02";
   self.estimatedBruteForceComputationTime = @"7.91e-02";
 }
