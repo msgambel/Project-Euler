451ef19258fd99a49e94bffc2e7e5725af6bf04c
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-11-07 16:30:06 -0500

    Added a Solution Line Count to Question 63.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
@@ -22,6 +22,7 @@
   self.rating = @"4";
   self.keywords = @"powers,digits,count,fifth,nine,9,exist,positive,integers,number,nth,bases,exponents";
   self.difficulty = @"Easy";
+  self.solutionLineCount = @"17";
   self.estimatedComputationTime = @"2.8e-05";
   self.estimatedBruteForceComputationTime = @"2.8e-05";
 }
