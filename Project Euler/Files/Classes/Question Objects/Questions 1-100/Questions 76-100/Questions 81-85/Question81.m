aaf543e72a1947ed99477734afe9344b8ead81c8
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-11-25 14:07:00 -0500

    Added a Solution Line Count to Question 81.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question81.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question81.m
@@ -23,6 +23,7 @@
   self.rating = @"4";
   self.keywords = @"matrix,a*,a,star,minimal,path,sum,import,80,eighty,2,two,ways,files,matrix";
   self.difficulty = @"Easy";
+  self.solutionLineCount = @"69";
   self.estimatedComputationTime = @"2.73e-02";
   self.estimatedBruteForceComputationTime = @"2.73e-02";
 }
