b284d2d6d7d73277639e7a10fcf830502a6dda41
Michael Sgambelluri <michaelsgam10@hotmail.com>
2015-12-28 17:41:06 -0500

    Added a Solve Time to Question 2.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question2.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question2.m
@@ -21,6 +21,7 @@
   self.number = @"2";
   self.rating = @"4";
   self.keywords = @"fibonacci,sequence,numbers,sum,even,valued,terms,exceed,four,million,4000000,odd,positive,generated,previous";
+  self.solveTime = @"60";
   self.difficulty = @"Meh";
   self.solutionLineCount = @"11";
   self.estimatedComputationTime = @"2.1e-05";
