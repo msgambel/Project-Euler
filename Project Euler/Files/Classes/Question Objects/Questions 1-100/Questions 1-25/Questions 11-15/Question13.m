55e36d113582ca8a45507d32c6f39e0d12479a9f
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-01-08 03:55:03 -0500

    Added a Solve Time to Question 13.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question13.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question13.m
@@ -28,6 +28,7 @@
   self.number = @"13";
   self.rating = @"4";
   self.keywords = @"sum,big,int,large,first,digits,10,ten,150,one,hundred,and,fifty,numbers,list,work,out";
+  self.solveTime = @"30";
   self.difficulty = @"Meh";
   self.solutionLineCount = @"13";
   self.estimatedComputationTime = @"2.86e-03";
