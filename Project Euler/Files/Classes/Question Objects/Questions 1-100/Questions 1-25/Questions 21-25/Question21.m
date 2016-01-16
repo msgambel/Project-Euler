552dafcc717a6c9486c3a1513e3f966ba29c9168
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-01-16 17:36:36 -0500

    Added a Solve Time to Question 21.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question21.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question21.m
@@ -22,6 +22,7 @@
   self.number = @"21";
   self.rating = @"5";
   self.keywords = @"divisors,proper,amicable,pair,numbers,sum,10000,ten,thousand,evalute,under,prime,factorization";
+  self.solveTime = @"30";
   self.difficulty = @"Easy";
   self.solutionLineCount = @"57";
   self.estimatedComputationTime = @"1.71e-02";
