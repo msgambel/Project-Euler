4fe821373243ce261f404ee8531f395385a1bb18
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-01-25 03:04:20 -0500

    Added a Solve Time to Question 30.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question30.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question30.m
@@ -27,6 +27,7 @@
   self.number = @"30";
   self.rating = @"3";
   self.keywords = @"sum,powers,digits,fourth,fifth,written,total,200000,two,hundred,thousand,5,five";
+  self.solveTime = @"60";
   self.difficulty = @"Easy";
   self.solutionLineCount = @"13";
   self.estimatedComputationTime = @"5.17e-02";
