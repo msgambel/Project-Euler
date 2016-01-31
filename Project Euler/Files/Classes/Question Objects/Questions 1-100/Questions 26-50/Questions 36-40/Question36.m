8d4d1eb40f829ef4fc7178ecb5f66b4a0f3c801d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-01-31 03:54:53 -0500

    Added a Solve Time to Question 36.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question36.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question36.m
@@ -27,6 +27,7 @@
   self.number = @"36";
   self.rating = @"5";
   self.keywords = @"Double-base,double,base,palindromes,palindromic,2,two,10,ten,decimal,number,one,million,1000000,less,leading,zeros";
+  self.solveTime = @"150";
   self.difficulty = @"Easy";
   self.solutionLineCount = @"53";
   self.estimatedComputationTime = @"7.61e-02";
