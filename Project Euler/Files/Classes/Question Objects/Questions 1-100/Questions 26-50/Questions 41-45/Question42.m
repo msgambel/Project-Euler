e214534fbd9715bfab32c6ddf4dfa59132691541
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-02-06 16:17:49 -0500

    Added a Solve Time to Question 42.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question42.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question42.m
@@ -22,6 +22,7 @@
   self.number = @"42";
   self.rating = @"3";
   self.keywords = @"sum,triangle,numbers,english,words,import,coded,sequence,2000,two,thousand,common,alphabetical,maximum,compute,value,many";
+  self.solveTime = @"60";
   self.difficulty = @"Easy";
   self.solutionLineCount = @"25";
   self.estimatedComputationTime = @"1.59e-02";
