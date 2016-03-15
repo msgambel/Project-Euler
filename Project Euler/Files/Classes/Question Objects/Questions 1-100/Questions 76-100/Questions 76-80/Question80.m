be4851325de254b8d069d29eb3f91bd1cd56e5b7
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-03-15 17:24:32 -0400

    Added a Solve Time to Question 80.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question80.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question80.m
@@ -22,6 +22,7 @@
   self.number = @"80";
   self.rating = @"4";
   self.keywords = @"irrational,square,root,digital,expansion,sums,natural,number,infinite,total,digits";
+  self.solveTime = @"60";
   self.difficulty = @"Easy";
   self.solutionLineCount = @"15";
   self.estimatedComputationTime = @"0.5";
