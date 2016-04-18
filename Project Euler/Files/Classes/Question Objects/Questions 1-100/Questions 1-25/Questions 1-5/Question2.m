a15d8bf396d72a95eecbbca0a132265e1bad31ac
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-04-18 03:39:48 -0400

    Added a Completed On Date to Question 2.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question2.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question2.m
@@ -23,6 +23,7 @@
   self.keywords = @"fibonacci,sequence,numbers,sum,even,valued,terms,exceed,four,million,4000000,odd,positive,generated,previous";
   self.solveTime = @"60";
   self.difficulty = @"Meh";
+  self.completedOnDate = @"02/01/13";
   self.solutionLineCount = @"11";
   self.estimatedComputationTime = @"2.1e-05";
   self.estimatedBruteForceComputationTime = @"4.7e-05";
