ce225e1a89d2ccecfff9443f4eee4a1b9d99c147
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-04-30 03:39:56 -0400

    Added a Completed On Date to Question 14.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question14.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question14.m
@@ -23,6 +23,7 @@
   self.keywords = @"chain,collatz,longest,sequence,iterative,positive,integers,one,million,1000000,starting,number,terms,proved,problem,allowed,above";
   self.solveTime = @"90";
   self.difficulty = @"Meh";
+  self.completedOnDate = @"14/01/13";
   self.solutionLineCount = @"37";
   self.estimatedComputationTime = @"0.541";
   self.estimatedBruteForceComputationTime = @"1.41";
