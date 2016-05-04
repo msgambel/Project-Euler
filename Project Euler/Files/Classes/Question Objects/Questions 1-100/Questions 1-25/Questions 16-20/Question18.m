fe4bcb43fdba9e573dd804e9f4f3701e0bdd8002
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-05-04 15:54:19 -0400

    Added a Completed On Date to Question 18.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question18.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question18.m
@@ -24,6 +24,7 @@
   self.keywords = @"path,sum,maximum,route,import,triangle,adjacent,pair,problem,15,fifteen,rows,simple,challenge,67,sixty,seven,top,bottom";
   self.solveTime = @"120";
   self.difficulty = @"Meh";
+  self.completedOnDate = @"18/01/13";
   self.solutionLineCount = @"33";
   self.estimatedComputationTime = @"5.25e-04";
   self.estimatedBruteForceComputationTime = @"5.25e-04";
