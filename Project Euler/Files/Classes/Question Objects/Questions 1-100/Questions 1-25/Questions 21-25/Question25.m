7acc8686b96b920197e08134a4b778249e417739
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-05-11 06:03:25 -0400

    Added a Completed On Date to Question 25.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question25.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question25.m
@@ -24,6 +24,7 @@
   self.keywords = @"fibonacci,closed,form,recurrance,terms,sequence,digits,contains,1000,one,thousand,first,number,expression,logarithm";
   self.solveTime = @"30";
   self.difficulty = @"Easy";
+  self.completedOnDate = @"25/01/13";
   self.solutionLineCount = @"3";
   self.estimatedComputationTime = @"4.6e-05";
   self.estimatedBruteForceComputationTime = @"3.25e-02";
