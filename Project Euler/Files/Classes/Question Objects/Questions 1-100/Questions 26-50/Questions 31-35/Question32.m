e8657d6add5a3a327249ed04a88ba049988b2264
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-05-18 04:12:22 -0400

    Added a Completed On Date to Question 32.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question32.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question32.m
@@ -23,6 +23,7 @@
   self.keywords = @"pandigital,sum,all,digit,multiplicand,multiplier,products,identity,written,containing,obtained";
   self.solveTime = @"60";
   self.difficulty = @"Easy";
+  self.completedOnDate = @"01/02/13";
   self.solutionLineCount = @"37";
   self.estimatedComputationTime = @"7.15e-03";
   self.estimatedBruteForceComputationTime = @"7.55e-02";
