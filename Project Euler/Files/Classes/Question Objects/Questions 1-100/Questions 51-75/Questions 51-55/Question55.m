54a232314f191b3f822b71aeed6d5702c69948af
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-06-10 03:19:31 -0400

    Added a Completed On Date to Question 55.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question55.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question55.m
@@ -24,6 +24,7 @@
   self.keywords = @"lychrel,numbers,palindrome,sum,add,reverse,iterations,below,10000,ten,thousand,50,fifty,palindromic,theoretical,nature,producing";
   self.solveTime = @"300";
   self.difficulty = @"Easy";
+  self.completedOnDate = @"24/02/13";
   self.solutionLineCount = @"43";
   self.estimatedComputationTime = @"1.67";
   self.estimatedBruteForceComputationTime = @"12.5";
