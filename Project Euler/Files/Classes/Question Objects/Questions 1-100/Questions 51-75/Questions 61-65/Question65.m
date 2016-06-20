6bb6b159d50bd6b530962f08717a70f27b16d79d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-06-20 04:42:44 -0400

    Added a Completed On Date to Question 65.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question65.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question65.m
@@ -24,6 +24,7 @@
   self.keywords = @"convergents,e,infinite,continued,fractions,digits,sum,numerator,100,one,hundred,rational,approximations,sequence,partial";
   self.solveTime = @"30";
   self.difficulty = @"Medium";
+  self.completedOnDate = @"06/03/13";
   self.solutionLineCount = @"15";
   self.estimatedComputationTime = @"1.27e-03";
   self.estimatedBruteForceComputationTime = @"1.27e-03";
