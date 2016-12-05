decd812cf7c3d3e2b39bf8c8a9c35c1720ede8b6
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-12-05 16:36:04 -0500

    Added a isChallenging to Question 5.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question5.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question5.m
@@ -24,6 +24,7 @@
   self.keywords = @"divisible,factorial,evenly,smallest,positive,numbers,20,twenty,remainder,primes,maximum,divisible,without";
   self.solveTime = @"30";
   self.difficulty = @"Meh";
+  self.isChallenging = NO;
   self.completedOnDate = @"05/01/13";
   self.solutionLineCount = @"7";
   self.estimatedComputationTime = @"3.9e-05";
