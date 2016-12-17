9d0b92745ea568f788ffd4834b5e9d5ae740042f
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-12-17 05:23:07 -0500

    Added a isChallenging to Question 17.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question17.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question17.m
@@ -32,6 +32,7 @@
   self.keywords = @"letters,sum,words,numbers,counts,british,usage,written,one,thousand,1000,contains,spaces,hyphens,compliance";
   self.solveTime = @"90";
   self.difficulty = @"Meh";
+  self.isChallenging = NO;
   self.completedOnDate = @"17/01/13";
   self.solutionLineCount = @"17";
   self.estimatedComputationTime = @"2.5e-05";
