ee38b76378e080bd4878d94614f0a080f0cc3b4f
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-12-19 14:44:26 -0500

    Added a isChallenging to Question 19.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
@@ -25,6 +25,7 @@
   self.keywords = @"week,month,day,counting,leap,year,twentieth,century,sundays,first,sum,total,research,divisible,fell";
   self.solveTime = @"60";
   self.difficulty = @"Meh";
+  self.isChallenging = NO;
   self.completedOnDate = @"19/01/13";
   self.solutionLineCount = @"23";
   self.estimatedComputationTime = @"1.86e-04";
