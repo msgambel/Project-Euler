223fe5d12aa1bff413e3227a0932fffc9620a761
Michael Sgambelluri <michaelsgam10@hotmail.com>
2016-12-28 05:46:24 -0500

    Added a isChallenging to Question 28.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question28.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question28.m
@@ -24,6 +24,7 @@
   self.keywords = @"corner,side,length,spiral,diagonals,clockwise,direction,1001,one,thousand,sum,numbers,formed,same,way";
   self.solveTime = @"90";
   self.difficulty = @"Easy";
+  self.isChallenging = NO;
   self.completedOnDate = @"28/01/13";
   self.solutionLineCount = @"11";
   self.estimatedComputationTime = @"2.9e-05";
