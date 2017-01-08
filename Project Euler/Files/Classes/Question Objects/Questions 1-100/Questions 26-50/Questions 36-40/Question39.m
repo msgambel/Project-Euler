200807f031d7e7a9914fdd2b8a43a3fa26779366
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-01-08 04:41:53 -0500

    Added a isChallenging to Question 39.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question39.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question39.m
@@ -24,6 +24,7 @@
   self.keywords = @"pythagorean,triple,perimeter,right,angle,length,sides,maximized,integral,1000,one,thousand,solutions,integer,triangles,pair,maximum";
   self.solveTime = @"60";
   self.difficulty = @"Easy";
+  self.isChallenging = NO;
   self.completedOnDate = @"08/02/13";
   self.solutionLineCount = @"27";
   self.estimatedComputationTime = @"2.43e-03";
