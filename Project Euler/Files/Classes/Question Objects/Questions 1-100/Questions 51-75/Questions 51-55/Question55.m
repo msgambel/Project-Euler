3de84b4e68356ba3340609b44c41a8a1046aba0e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-01-24 03:13:09 -0500

    Added a isChallenging to Question 55.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question55.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question55.m
@@ -25,6 +25,7 @@
   self.keywords = @"lychrel,numbers,palindrome,sum,add,reverse,iterations,below,10000,ten,thousand,50,fifty,palindromic,theoretical,nature,producing";
   self.solveTime = @"300";
   self.difficulty = @"Easy";
+  self.isChallenging = NO;
   self.completedOnDate = @"24/02/13";
   self.solutionLineCount = @"43";
   self.estimatedComputationTime = @"1.67";
