93182fe354f77687ed8ec9e826193284cb63744d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-02-04 07:52:45 -0500

    Added a isChallenging to Question 66.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question66.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question66.m
@@ -24,6 +24,7 @@
   self.keywords = @"diophantine,equations,continued,fractions,expansions,pells,periods,minimal,solutions,largest,value,quadratic,length";
   self.solveTime = @"300";
   self.difficulty = @"Medium";
+  self.isChallenging = YES;
   self.completedOnDate = @"07/03/13";
   self.solutionLineCount = @"47";
   self.estimatedComputationTime = @"6.31e-04";
