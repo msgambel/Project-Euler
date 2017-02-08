166ee41282962577cab4cda728354282480a1fdc
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-02-08 03:34:20 -0500

    Added a isChallenging to Question 70.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question70.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question70.m
@@ -25,6 +25,7 @@
   self.keywords = @"totient,function,maximum,phi,euler's,eulers,product,permutation,relatively,prime,positive,numbers,ratio,produces,minimum";
   self.solveTime = @"90";
   self.difficulty = @"Medium";
+  self.isChallenging = YES;
   self.completedOnDate = @"11/03/13";
   self.solutionLineCount = @"91";
   self.estimatedComputationTime = @"1.83e-02";
