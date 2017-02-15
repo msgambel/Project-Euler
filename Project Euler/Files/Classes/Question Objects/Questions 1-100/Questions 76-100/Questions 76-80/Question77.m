c8791ac5fc92f386714d4191d2a701caa0a6e310
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-02-15 14:31:45 -0500

    Added a isChallenging to Question 77.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question77.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question77.m
@@ -24,6 +24,7 @@
   self.keywords = @"primes,summations,five,thousand,5000,positive,different,permutations,ways,written,as,sum,possible";
   self.solveTime = @"90";
   self.difficulty = @"Easy";
+  self.isChallenging = NO;
   self.completedOnDate = @"18/03/13";
   self.solutionLineCount = @"51";
   self.estimatedComputationTime = @"4.84e-04";
