b08cffd50c6aece6fd584ed6c4f6544e2d516bbe
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-02-19 06:25:28 -0500

    Added a isChallenging to Question 81.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question81.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question81.m
@@ -25,6 +25,7 @@
   self.keywords = @"matrix,a*,a,star,minimal,path,sum,import,80,eighty,2,two,ways,files,matrix";
   self.solveTime = @"60";
   self.difficulty = @"Easy";
+  self.isChallenging = NO;
   self.completedOnDate = @"22/03/13";
   self.solutionLineCount = @"69";
   self.estimatedComputationTime = @"2.73e-02";
