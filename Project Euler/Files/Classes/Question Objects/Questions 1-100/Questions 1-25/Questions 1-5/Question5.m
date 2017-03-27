12a09a08144038b899a4043f0006b5693cc0b70f
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-03-27 07:05:00 -0400

    Added a Link to Question 5.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question5.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question5.m
@@ -14,6 +14,7 @@
   
   self.date = @"30 November 2001";
   self.hint = @"Factorials include all the numbers from 1 to n. They also include all the primes from 1 to n. So what do the maximum prime powers give us?";
+  self.link = @"https://en.wikipedia.org/wiki/Factorial";
   self.text = @"2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.\n\nWhat is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?";
   self.isFun = YES;
   self.title = @"Smallest multiple";
