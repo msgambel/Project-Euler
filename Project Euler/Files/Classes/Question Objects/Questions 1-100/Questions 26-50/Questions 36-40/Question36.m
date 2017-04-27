d12e9d4c4804042a9e26182ae79a0bfd3935f7bf
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-04-27 17:43:10 -0400

    Added a Link to Question 36.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question36.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question36.m
@@ -20,6 +20,7 @@
   
   self.date = @"31 January 2003";
   self.hint = @"Instead of checking if a base 10 number is a palindrome, write a function that generates all base 10 palindromes less that 1,000,000, and check if those palindromes are also base 2 palindromes.";
+  self.link = @"https://en.wikipedia.org/wiki/Palindrome";
   self.text = @"The decimal number, 585 = 1001001001_{2} (binary), is palindromic in both bases.\n\nFind the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.\n\n(Please note that the palindromic number, in either base, may not include leading zeros.)";
   self.isFun = YES;
   self.title = @"Double-base palindromes";
