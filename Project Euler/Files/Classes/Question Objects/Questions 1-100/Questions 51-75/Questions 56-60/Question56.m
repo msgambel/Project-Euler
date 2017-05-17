006513b8f75e68aa62e129dfc20005f7e4ad909d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-05-17 05:16:01 -0400

    Added a Link to Question 56.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question56.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question56.m
@@ -15,6 +15,7 @@
   
   self.date = @"07 November 2003";
   self.hint = @"Ignore powers of 10, and b <= 90.";
+  self.link = @"https://docs.microsoft.com/en-us/sql/t-sql/data-types/int-bigint-smallint-and-tinyint-transact-sql";
   self.text = @"A googol (10^100) is a massive number: one followed by one-hundred zeros; 100^100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.\n\nConsidering natural numbers of the form, a^b, where a, b <= 100, what is the maximum digital sum?";
   self.isFun = YES;
   self.title = @"Powerful digit sum";
