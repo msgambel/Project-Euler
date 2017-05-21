bcff9c0f712ec6933f5100854e2e31efbf8136ec
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-05-21 03:38:22 -0400

    Added a Link to Question 60.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question60.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question60.m
@@ -23,6 +23,7 @@
   
   self.date = @"02 January 2004";
   self.hint = @"All primes in a set must be unique.";
+  self.link = @"https://en.wikipedia.org/wiki/Primality_test#Pseudocode";
   self.text = @"The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.\n\nFind the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.";
   self.isFun = YES;
   self.title = @"Prime pair sets";
