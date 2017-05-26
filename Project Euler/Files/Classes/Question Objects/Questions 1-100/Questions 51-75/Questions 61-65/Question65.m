64fca92cd43f8a3d113edc446452dc8fdab68f82
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-05-26 15:24:33 -0400

    Added a Link to Question 65.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question65.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question65.m
@@ -15,6 +15,7 @@
   
   self.date = @"12 March 2004";
   self.hint = @"Find the sequence of numerators and denominators by induction.";
+  self.link = @"http://mathworld.wolfram.com/RecursiveSequence.html";
   self.text = @"The square root of 2 can be written as an infinite continued fraction.\n\n(Please visit the website, as UITextView's cannot render any of the required font!)\n\nThe infinite continued fraction can be written, 2 = [1;(2)], (2) indicates that 2 repeats ad infinitum. In a similar way, 23 = [4;(1,3,1,8)].\n\nIt turns out that the sequence of partial values of continued fractions for square roots provide the best rational approximations. Let us consider the convergents for 2.\n\n(Please visit site, as UITextView's cannot render any of the required font!)\n\nHence the sequence of the first ten convergents for 2 are:\n\n1, 3/2, 7/5, 17/12, 41/29, 99/70, 239/169, 577/408, 1393/985, 3363/2378, ...\n\nWhat is most surprising is that the important mathematical constant,\n\ne = [2; 1,2,1, 1,4,1, 1,6,1 , ... , 1,2k,1, ...].\n\nThe first ten terms in the sequence of convergents for e are:\n\n2, 3, 8/3, 11/4, 19/7, 87/32, 106/39, 193/71, 1264/465, 1457/536, ...\n\nThe sum of digits in the numerator of the 10th convergent is 1+4+5+7=17.\n\nFind the sum of digits in the numerator of the 100th convergent of the continued fraction for e.";
   self.isFun = NO;
   self.title = @"Convergents of e";
