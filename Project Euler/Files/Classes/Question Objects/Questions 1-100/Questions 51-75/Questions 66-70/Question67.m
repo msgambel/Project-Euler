d1582ebd8616c34f42b29421a763f164b1c401f9
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-05-28 05:14:26 -0400

    Added a Link to Question 67.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question67.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question67.m
@@ -15,6 +15,7 @@
   
   self.date = @"09 April 2004";
   self.hint = @"Don't go top to bottom, go bottom to top!";
+  self.link = @"https://en.wikipedia.org/wiki/Binary_tree";
   self.text = @"By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.\n\n\n3\n7 4\n2 4 6\n8 5 9 3\n\n\nThat is, 3 + 7 + 4 + 9 = 23.\n\n\nFind the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.\n\nNOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether! If you could check one trillion (10^12) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)";
   self.isFun = YES;
   self.title = @"Maximum path sum II";
