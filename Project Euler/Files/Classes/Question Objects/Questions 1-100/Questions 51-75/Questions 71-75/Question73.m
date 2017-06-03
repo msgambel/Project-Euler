1640842966b7398328957b37329a282353658ca3
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-06-03 04:14:22 -0400

    Added a Link to Question 73.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question73.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question73.m
@@ -15,6 +15,7 @@
   
   self.date = @"02 July 2004";
   self.hint = @"Use the Euler Totient Function to generate all of the numbers that are relatively prime to a given number. Also, 2 | φ(n) for all n >= 3.";
+  self.link = @"http://en.wikipedia.org/wiki/Euler's_totient_function#Euler.27s_product_formula";
   self.text = @"Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a reduced proper fraction.\n\nIf we list the set of reduced proper fractions for d <= 8 in ascending order of size, we get:\n\n1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8\n\nIt can be seen that there are 3 fractions between 1/3 and 1/2.\n\nHow many fractions lie between 1/3 and 1/2 in the sorted set of reduced proper fractions for d <= 12,000?";
   self.isFun = YES;
   self.title = @"Counting fractions in a range";
