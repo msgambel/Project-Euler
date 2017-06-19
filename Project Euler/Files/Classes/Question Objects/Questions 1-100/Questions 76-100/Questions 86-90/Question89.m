713911203815b59f4601112d60bd90fc27014218
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-06-19 04:50:45 -0400

    Added a Link to Question 89.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question89.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question89.m
@@ -23,6 +23,7 @@
   
   self.date = @"18 February 2005";
   self.hint = @"Make a helper method that reduces a Roman numeral to its minimal form.";
+  self.link = @"https://en.wikipedia.org/wiki/Roman_numerals";
   self.text = @"The rules for writing Roman numerals allow for many ways of writing each number (see About Roman Numerals...). However, there is always a \"best\" way of writing a particular number.\n\nFor example, the following represent all of the legitimate ways of writing the number sixteen:\n\nIIIIIIIIIIIIIIII\nVIIIIIIIIIII\nVVIIIIII\nXIIIIII\nVVVI\nXVI\n\nThe last example being considered the most efficient, as it uses the least number of numerals.\n\nThe 11K text file, roman.txt (right click and 'Save Link/Target As...'), contains one thousand numbers written in valid, but not necessarily minimal, Roman numerals; that is, they are arranged in descending units and obey the subtractive pair rule (see About Roman Numerals... for the definitive rules for this problem).\n\nFind the number of characters saved by writing each of these in their minimal form.\n\nNote: You can assume that all the Roman numerals in the file contain no more than four consecutive identical units.";
   self.isFun = YES;
   self.title = @"Roman numerals";
