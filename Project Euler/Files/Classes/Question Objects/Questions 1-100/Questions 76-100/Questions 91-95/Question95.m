89958326d5dde652d9e9294ea05996b742f82cff
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-06-25 03:27:25 -0400

    Added a Link to Question 95.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question95.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question95.m
@@ -15,6 +15,7 @@
   
   self.date = @"13 May 2005";
   self.hint = @"Find the sum of the proper factors, and mark the known cycle numbers.";
+  self.link = @"https://en.wikipedia.org/wiki/Prime_factor";
   self.text = @"The proper divisors of a number are all the divisors excluding the number itself. For example, the proper divisors of 28 are 1, 2, 4, 7, and 14. As the sum of these divisors is equal to 28, we call it a perfect number.\n\nInterestingly the sum of the proper divisors of 220 is 284 and the sum of the proper divisors of 284 is 220, forming a chain of two numbers. For this reason, 220 and 284 are called an amicable pair.\n\nPerhaps less well known are longer chains. For example, starting with 12496, we form a chain of five numbers:\n\n12496 -> 14288 -> 15472 -> 14536 -> 14264 (-> 12496 -> ...)\n\nSince this chain returns to its starting point, it is called an amicable chain.\n\nFind the smallest member of the longest amicable chain with no element exceeding one million.";
   self.isFun = YES;
   self.title = @"Amicable chains";
