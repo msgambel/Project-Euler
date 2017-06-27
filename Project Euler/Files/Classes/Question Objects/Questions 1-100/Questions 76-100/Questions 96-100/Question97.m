e5ebff8bd16663a06e99e8b50d31c772687bba71
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-06-27 06:13:24 -0400

    Added a Link to Question 97.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 96-100/Question97.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 96-100/Question97.m
@@ -15,6 +15,7 @@
   
   self.date = @"10 June 2005";
   self.hint = @"BigInt classes are your friend!";
+  self.link = @"https://docs.microsoft.com/en-us/sql/t-sql/data-types/int-bigint-smallint-and-tinyint-transact-sql";
   self.text = @"The first known prime found to exceed one million digits was discovered in 1999, and is a Mersenne prime of the form 2^6972593-1; it contains exactly 2,098,960 digits. Subsequently other Mersenne primes, of the form 2^p-1, have been found which contain more digits.\n\nHowever, in 2004 there was found a massive non-Mersenne prime which contains 2,357,207 digits: 28433x2^7830457+1.\n\nFind the last ten digits of this prime number.";
   self.isFun = YES;
   self.title = @"Large non-Mersenne prime";
