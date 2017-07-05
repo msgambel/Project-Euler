d32bfaff8adf6cb7d5a5c5fccc538a5a7e8ced4e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-07-05 03:02:12 -0400

    Added a Link to Question 187.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
@@ -14,6 +14,7 @@
   
   self.date = @"22 March 2008";
   self.hint = @"Get a list of primes to create the required semiprimes!";
+  self.link = @"https://en.wikipedia.org/wiki/Semiprime";
   self.text = @"A composite is a number containing at least two prime factors. For example, 15 = 3 * 5; 9 = 3 * 3; 12 = 2 * 2 * 3.\n\nThere are ten composites below thirty containing precisely two, not necessarily distinct, prime factors: 4, 6, 9, 10, 14, 15, 21, 22, 25, 26.\nHow many composite integers, n < 10^8, have precisely two, not necessarily distinct, prime factors?";
   self.isFun = YES;
   self.title = @"Semiprimes";
