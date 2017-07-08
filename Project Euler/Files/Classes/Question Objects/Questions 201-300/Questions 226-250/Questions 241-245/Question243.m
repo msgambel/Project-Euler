5177a3c1ac31ae5deda8b42985595809450b9ce9
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-07-08 03:43:56 -0400

    Added a Link to Question 243.

--- Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 226-250/Questions 241-245/Question243.m
+++ Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 226-250/Questions 241-245/Question243.m
@@ -14,6 +14,7 @@
   
   self.date = @"02 May 2009";
   self.hint = @"Only unique prime factors matter.";
+  self.link = @"https://en.wikipedia.org/wiki/Fundamental_theorem_of_arithmetic";
   self.text = @"A positive fraction whose numerator is less than its denominator is called a proper fraction.\n\nFor any denominator, d, there will be d1 proper fractions; for example, with d = 12:\n\n1/12 , 2/12 , 3/12 , 4/12 , 5/12 , 6/12 , 7/12 , 8/12 , 9/12 , 10/12 , 11/12.\n\nWe shall call a fraction that cannot be cancelled down a resilient fraction.\n\nFurthermore we shall define the resilience of a denominator, R(d), to be the ratio of its proper fractions that are resilient; for example, R(12) = 4/11.\n\nIn fact, d = 12 is the smallest denominator having a resilience R(d) < 4/10.\n\nFind the smallest denominator d, having a resilience R(d) < 15499/94744.";
   self.isFun = YES;
   self.title = @"Resilience";
