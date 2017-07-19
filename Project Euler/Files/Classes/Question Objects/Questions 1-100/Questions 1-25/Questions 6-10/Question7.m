f19f33bdc3b68e891e1f03b617f62447179ae0eb
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-07-19 04:31:39 -0400

    Added a Technique to Question 7.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question7.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question7.m
@@ -24,6 +24,7 @@
   self.category = @"Primes";
   self.keywords = @"prime,listing,10001st,one,thousand,and,find,number,first,helper,method";
   self.solveTime = @"90";
+  self.technique = @"Math";
   self.difficulty = @"Meh";
   self.isChallenging = NO;
   self.completedOnDate = @"07/01/13";
