e86cec05dfeb437604bb1ae14f3c71433b40d0a8
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-08-21 06:42:52 -0400

    Added a Technique to Question 40.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question40.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question40.m
@@ -24,6 +24,7 @@
   self.category = @"Combinations";
   self.keywords = @"concatenate,digits,constant,champernowne's,positive,integers,expression,fractional,part,number,value";
   self.solveTime = @"300";
+  self.technique = @"Recursion";
   self.difficulty = @"Medium";
   self.isChallenging = YES;
   self.completedOnDate = @"09/02/13";
