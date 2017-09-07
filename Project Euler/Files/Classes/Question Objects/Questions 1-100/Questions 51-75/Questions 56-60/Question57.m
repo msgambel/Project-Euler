6e50a3c65d9d7a1136b3ac82d63ac59d93019156
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-09-07 12:03:15 -0400

    Added a Technique to Question 57.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question57.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question57.m
@@ -25,6 +25,7 @@
   self.category = @"Patterns";
   self.keywords = @"square,root,continued,fractions,infinite,expansions,numerator,denominator,one,thousand,1000,more,digits,how,many";
   self.solveTime = @"60";
+  self.technique = @"Math";
   self.difficulty = @"Medium";
   self.isChallenging = YES;
   self.completedOnDate = @"26/02/13";
