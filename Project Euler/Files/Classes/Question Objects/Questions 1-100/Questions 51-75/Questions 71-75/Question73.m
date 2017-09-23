5c455579b6c03f83fe7dbd1bbe8a781c2c34995e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-09-23 07:01:35 -0400

    Added a Technique to Question 73.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question73.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question73.m
@@ -25,6 +25,7 @@
   self.category = @"Counting";
   self.keywords = @"counting,fractions,reduced,proper,ascending,order,interval,positive,integers,HCF,sorted,euler,totient";
   self.solveTime = @"300";
+  self.technique = @"Math";
   self.difficulty = @"Medium";
   self.isChallenging = YES;
   self.completedOnDate = @"14/03/13";
