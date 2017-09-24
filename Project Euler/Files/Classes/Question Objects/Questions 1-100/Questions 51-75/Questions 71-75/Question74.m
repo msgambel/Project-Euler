89f7c858193e0994462d0cf3c3ee8008bce19a4a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-09-24 05:09:10 -0400

    Added a Technique to Question 74.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question74.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question74.m
@@ -24,6 +24,7 @@
   self.category = @"Patterns";
   self.keywords = @"digit,factorial,chains,length,loop,sixty,60,unique,property,non-repeating,terms,contain,exactly";
   self.solveTime = @"30";
+  self.technique = @"Recursion";
   self.difficulty = @"Easy";
   self.isChallenging = NO;
   self.completedOnDate = @"15/03/13";
