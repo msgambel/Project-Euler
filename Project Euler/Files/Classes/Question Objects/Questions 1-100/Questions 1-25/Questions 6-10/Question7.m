7b47ea395df7686e1b74be2b0dacec68bdcfe2b6
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-11-08 05:04:17 -0500

    Added a Comment Count to Question 7.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question7.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question7.m
@@ -26,6 +26,7 @@
   self.solveTime = @"90";
   self.technique = @"Math";
   self.difficulty = @"Meh";
+  self.commentCount = @"10";
   self.isChallenging = NO;
   self.completedOnDate = @"07/01/13";
   self.solutionLineCount = @"41";
