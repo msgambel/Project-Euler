8d50e1d2bc7acea4033fd793fece352ee957a38d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-11-22 05:13:50 -0500

    Added a Comment Count to Question 21.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question21.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question21.m
@@ -27,6 +27,7 @@
   self.solveTime = @"30";
   self.technique = @"Math";
   self.difficulty = @"Easy";
+  self.commentCount = @"64";
   self.isChallenging = NO;
   self.completedOnDate = @"21/01/13";
   self.solutionLineCount = @"57";
