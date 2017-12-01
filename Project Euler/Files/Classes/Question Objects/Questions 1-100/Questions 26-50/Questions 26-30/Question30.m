ce1462b395974470acaa41ee6c6d24226244c055
Michael Sgambelluri <michaelsgam10@hotmail.com>
2017-12-01 10:27:22 -0500

    Added a Comment Count to Question 30.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question30.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question30.m
@@ -32,6 +32,7 @@
   self.solveTime = @"60";
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
+  self.commentCount = @"12";
   self.isChallenging = NO;
   self.completedOnDate = @"30/01/13";
   self.solutionLineCount = @"13";
