fab38417ed12023e985b4c28bfee7f41b6c6da82
Michael Sgambelluri <michaelsgam10@hotmail.com>
2018-01-18 08:04:30 -0500

    Added a Comment Count to Question 78.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question78.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question78.m
@@ -27,6 +27,7 @@
   self.solveTime = @"300";
   self.technique = @"Math";
   self.difficulty = @"Easy";
+  self.commentCount = @"35";
   self.isChallenging = NO;
   self.completedOnDate = @"19/03/13";
   self.solutionLineCount = @"29";
