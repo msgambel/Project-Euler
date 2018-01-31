478a19f820f215a1e620792710c1f1a46bdb5adb
Michael Sgambelluri <michaelsgam10@hotmail.com>
2018-01-31 03:54:16 -0500

    Added a Comment Count to Question 91.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question91.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question91.m
@@ -26,6 +26,7 @@
   self.solveTime = @"30";
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
+  self.commentCount = @"22";
   self.isChallenging = NO;
   self.completedOnDate = @"01/04/13";
   self.solutionLineCount = @"15";
