a686516e660a3b8ccfff4899e59eedfc0f15145a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2018-02-11 08:03:53 -0500

    Added a Comment Count to Question 120.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
@@ -26,6 +26,7 @@
   self.solveTime = @"60";
   self.technique = @"Math";
   self.difficulty = @"Easy";
+  self.commentCount = @"16";
   self.isChallenging = NO;
   self.completedOnDate = @"12/04/13";
   self.solutionLineCount = @"9";
