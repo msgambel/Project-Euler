4b04f49ee420727d43542081ef834cbfa795e0b8
Michael Sgambelluri <michaelsgam10@hotmail.com>
2018-02-13 18:03:01 -0500

    Added a Comment Count to Question 131.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 126-150/Questions 131-135/Question131.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 126-150/Questions 131-135/Question131.m
@@ -26,6 +26,7 @@
   self.solveTime = @"60";
   self.technique = @"Math";
   self.difficulty = @"Easy";
+  self.commentCount = @"18";
   self.isChallenging = NO;
   self.completedOnDate = @"14/04/13";
   self.solutionLineCount = @"37";
