7c5c33561b2f4f2d705d3058947a269f53734e73
Michael Sgambelluri <michaelsgam10@hotmail.com>
2018-02-14 04:21:12 -0500

    Added a Comment Count to Question 187.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
@@ -26,6 +26,7 @@
   self.solveTime = @"30";
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
+  self.commentCount = @"33";
   self.isChallenging = NO;
   self.completedOnDate = @"15/04/13";
   self.solutionLineCount = @"65";
