3105a52bf44ea485a6d833b0a9b4b87ebb949f85
Michael Sgambelluri <michaelsgam10@hotmail.com>
2018-02-15 05:04:16 -0500

    Added a Comment Count to Question 206.

--- Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question206.m
+++ Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question206.m
@@ -26,6 +26,7 @@
   self.solveTime = @"30";
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
+  self.commentCount = @"52";
   self.isChallenging = NO;
   self.completedOnDate = @"16/04/13";
   self.solutionLineCount = @"47";
