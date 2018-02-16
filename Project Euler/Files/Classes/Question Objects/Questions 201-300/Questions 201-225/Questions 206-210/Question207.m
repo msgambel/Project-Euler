3e33f2fc85c3c79061f4dc4963b473e9051b9fd1
Michael Sgambelluri <michaelsgam10@hotmail.com>
2018-02-16 05:53:31 -0500

    Added a Comment Count to Question 207.

--- Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question207.m
+++ Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question207.m
@@ -26,6 +26,7 @@
   self.solveTime = @"60";
   self.technique = @"Math";
   self.difficulty = @"Easy";
+  self.commentCount = @"19";
   self.isChallenging = NO;
   self.completedOnDate = @"17/04/13";
   self.solutionLineCount = @"11";
