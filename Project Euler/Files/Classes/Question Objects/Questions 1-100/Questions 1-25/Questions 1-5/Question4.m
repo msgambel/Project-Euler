33c868861dda566f15b0a6606d3cd327e0f744d2
Michael Sgambelluri <michaelsgam10@hotmail.com>
2018-02-25 03:47:04 -0500

    Added a Uses Helper Methods to Question 4.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question4.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question4.m
@@ -36,6 +36,7 @@
   self.isChallenging = NO;
   self.completedOnDate = @"04/01/13";
   self.solutionLineCount = @"15";
+  self.usesHelperMethods = YES;
   self.estimatedComputationTime = @"7.97e-04";
   self.estimatedBruteForceComputationTime = @"4.8e-02";
 }
