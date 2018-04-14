bd1c3167043b4b20c95f5799e04818244c6922e2
Michael Sgambelluri <michaelsgam10@hotmail.com>
2018-04-14 04:48:58 -0400

    Added a Uses Helper Methods to Question 52.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question52.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question52.m
@@ -30,6 +30,7 @@
   self.isChallenging = NO;
   self.completedOnDate = @"21/02/13";
   self.solutionLineCount = @"39";
+  self.usesHelperMethods = NO;
   self.estimatedComputationTime = @"3.11e-02";
   self.estimatedBruteForceComputationTime = @"5.63e-02";
 }
