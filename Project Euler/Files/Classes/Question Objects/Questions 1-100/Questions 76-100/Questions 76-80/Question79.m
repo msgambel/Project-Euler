ebd9ede5425aac246e017ec055499bdda260c685
Michael Sgambelluri <michaelsgam10@hotmail.com>
2018-05-11 07:34:23 -0400

    Added a Uses Helper Methods to Question 79.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question79.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question79.m
@@ -36,6 +36,7 @@
   self.isChallenging = NO;
   self.completedOnDate = @"20/03/13";
   self.solutionLineCount = @"39";
+  self.usesHelperMethods = NO;
   self.estimatedComputationTime = @"3.23e-04";
   self.estimatedBruteForceComputationTime = @"93.9";
 }
