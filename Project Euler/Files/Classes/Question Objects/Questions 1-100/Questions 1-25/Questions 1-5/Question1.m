791e06cdec272cb0bcfbe096ef2af97cf90bc286
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-01-24 06:56:56 -0500

    Added a Requires Mathematics to Question 1.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question1.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question1.m
@@ -38,6 +38,7 @@
   self.completedOnDate = @"01/01/13";
   self.solutionLineCount = @"11";
   self.usesHelperMethods = YES;
+  self.requiresMathematics = NO;
   self.hasMultipleSolutions = YES;
   self.estimatedComputationTime = @"2.1e-05";
   self.estimatedBruteForceComputationTime = @"2.7e-05";
