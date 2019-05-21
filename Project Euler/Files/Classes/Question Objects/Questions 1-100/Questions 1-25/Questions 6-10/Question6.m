49f674926dac6a25a2be67856195b1816eb86e35
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-05-21 04:44:44 -0400

    Added a Uses Functional Programming to Question 6.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question6.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question6.m
@@ -35,6 +35,7 @@
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = YES;
   self.estimatedComputationTime = @"2.3e-05";
+  self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"2.4e-05";
 }
 
