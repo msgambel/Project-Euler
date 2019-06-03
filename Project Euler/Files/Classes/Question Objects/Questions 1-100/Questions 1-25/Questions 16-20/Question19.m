0e312f1801eb3b27a55a7b426a5719c09e11834b
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-06-03 05:25:25 -0400

    Added a Uses Functional Programming to Question 19.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
@@ -36,6 +36,7 @@
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"1.86e-04";
+  self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"1.86e-04";
 }
 
