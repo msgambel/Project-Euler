2498fb78e674f2451df195a0e187a0080fc190c4
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-06-21 22:22:22 -0400

    Added a Uses Functional Programming to Question 37.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question37.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question37.m
@@ -44,6 +44,7 @@
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = YES;
   self.estimatedComputationTime = @"0.99";
+  self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"1.14";
 }
 
