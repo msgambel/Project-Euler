683cac1a5b1b31537f18836ab9d263c63caeef7c
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-07-20 04:44:44 -0400

    Added a Uses Functional Programming to Question 66.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question66.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question66.m
@@ -35,6 +35,7 @@
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"6.31e-04";
+  self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"6.31e-04";
 }
 
