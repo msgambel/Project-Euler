0739a44cf7a3d802407f4facd9dc182b550c129a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-08-30 04:04:04 -0400

    Added a Uses Functional Programming to Question 207.

--- Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question207.m
+++ Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question207.m
@@ -35,6 +35,7 @@
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"1.19e-02";
+  self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"1.19e-02";
 }
 
