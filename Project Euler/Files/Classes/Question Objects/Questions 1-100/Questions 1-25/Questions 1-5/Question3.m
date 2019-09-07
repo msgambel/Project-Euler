9b9b58f3aeb09ce8e39307495d453b881bf1788d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-09-07 05:55:55 -0400

    Added an Attempts Count to Question 3.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question3.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question3.m
@@ -27,6 +27,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Meh";
   self.commentCount = @"31";
+  self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"03/01/13";
   self.completedOnDate = @"03/01/13";
