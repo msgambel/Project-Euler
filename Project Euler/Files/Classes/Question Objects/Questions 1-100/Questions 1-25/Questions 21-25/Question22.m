faca2dbff494548f5cef71769717e472d87877e6
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-09-26 04:24:24 -0400

    Added an Attempts Count to Question 22.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question22.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question22.m
@@ -27,6 +27,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
   self.commentCount = @"15";
+  self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"22/01/13";
   self.completedOnDate = @"22/01/13";
