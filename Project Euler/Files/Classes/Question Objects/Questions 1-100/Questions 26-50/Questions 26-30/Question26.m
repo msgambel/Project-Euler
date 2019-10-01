8f790ab38cf1cb6fee5861ac532d2a8bd05af32a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-09-30 21:00:12 -0400

    Added an Attempts Count to Question 26.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question26.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question26.m
@@ -27,6 +27,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
   self.commentCount = @"41";
+  self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"26/01/13";
   self.completedOnDate = @"26/01/13";
