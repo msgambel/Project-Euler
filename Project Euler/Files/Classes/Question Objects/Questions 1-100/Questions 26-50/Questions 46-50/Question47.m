043d3dcfc379a1e1f9fb4fa54b351a41cdd321f8
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-10-21 04:44:44 -0400

    Added an Attempts Count to Question 47.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question47.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question47.m
@@ -27,6 +27,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
   self.commentCount = @"40";
+  self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"16/02/13";
   self.completedOnDate = @"16/02/13";
