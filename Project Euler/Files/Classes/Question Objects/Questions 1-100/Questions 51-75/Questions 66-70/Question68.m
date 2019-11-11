3d9d419e1314270253fd2c0e02a56d721716e1a4
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-11-11 09:29:29 -0500

    Added an Attempts Count to Question 68.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question68.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question68.m
@@ -27,6 +27,7 @@
   self.technique = @"Logic";
   self.difficulty = @"Medium";
   self.commentCount = @"8";
+  self.attemptsCount = @"3";
   self.isChallenging = YES;
   self.startedOnDate = @"09/03/13";
   self.completedOnDate = @"09/03/13";
