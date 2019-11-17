ca11be563246a90352f5ed4f0d01f38a5a983852
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-11-17 03:03:03 -0500

    Added an Attempts Count to Question 74.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question74.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question74.m
@@ -27,6 +27,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
   self.commentCount = @"34";
+  self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"15/03/13";
   self.completedOnDate = @"15/03/13";
