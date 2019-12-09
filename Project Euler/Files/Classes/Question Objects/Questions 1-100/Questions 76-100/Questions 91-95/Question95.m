354c09688df392f12881e424178ecbcdc0f52b54
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-12-08 20:00:02 -0500

    Added an Attempts Count to Question 95.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question95.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question95.m
@@ -28,6 +28,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Medium";
   self.commentCount = @"98";
+  self.attemptsCount = @"2";
   self.isChallenging = YES;
   self.startedOnDate = @"05/04/13";
   self.completedOnDate = @"05/04/13";
