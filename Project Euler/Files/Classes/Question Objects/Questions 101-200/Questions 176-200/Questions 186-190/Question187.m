37b3c96328b6d3b10a7ca1a33a7a0ebb049124bc
Michael Sgambelluri <michaelsgam10@hotmail.com>
2019-12-18 03:33:33 -0500

    Added an Attempts Count to Question 187.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
@@ -27,6 +27,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
   self.commentCount = @"33";
+  self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"15/04/13";
   self.completedOnDate = @"15/04/13";
