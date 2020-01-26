fa2979cd5b650396dfe180bc511953d3a8414ae7
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-01-26 03:03:03 -0500

    Added a Uses Custom Objects to Question 22.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question22.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question22.m
@@ -32,6 +32,7 @@
   self.startedOnDate = @"22/01/13";
   self.completedOnDate = @"22/01/13";
   self.solutionLineCount = @"17";
+  self.usesCustomObjects = NO;
   self.usesHelperMethods = YES;
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
