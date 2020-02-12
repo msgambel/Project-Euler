3e1d3e9be79823d6ed1bed3cac44e7830e84b85a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-02-12 03:03:03 -0500

    Added a Uses Custom Objects to Question 39.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question39.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question39.m
@@ -32,6 +32,7 @@
   self.startedOnDate = @"08/02/13";
   self.completedOnDate = @"08/02/13";
   self.solutionLineCount = @"27";
+  self.usesCustomObjects = NO;
   self.usesHelperMethods = YES;
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
