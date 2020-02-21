bcd67e38cb55f7b939a3c8d932d2e46360334c0e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-02-21 07:07:07 -0500

    Added a Uses Custom Objects to Question 48.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question48.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question48.m
@@ -33,6 +33,7 @@
   self.startedOnDate = @"17/02/13";
   self.completedOnDate = @"17/02/13";
   self.solutionLineCount = @"15";
+  self.usesCustomObjects = YES;
   self.usesHelperMethods = YES;
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = NO;
