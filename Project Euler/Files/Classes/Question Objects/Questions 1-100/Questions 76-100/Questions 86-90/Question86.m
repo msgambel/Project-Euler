e266007f297d7ef503518bac9a3bc411b2b3f5ca
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-03-30 05:05:05 -0400

    Added a Uses Custom Objects to Question 86.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question86.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question86.m
@@ -33,6 +33,7 @@
   self.startedOnDate = @"27/03/13";
   self.completedOnDate = @"27/03/13";
   self.solutionLineCount = @"17";
+  self.usesCustomObjects = NO;
   self.usesHelperMethods = YES;
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = NO;
