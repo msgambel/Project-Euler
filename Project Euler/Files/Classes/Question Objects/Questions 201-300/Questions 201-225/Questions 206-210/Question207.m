4e20c967f9eacaaa4b7b10e8aa18d75f931a5b14
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-04-20 03:03:03 -0400

    Added a Uses Custom Objects to Question 207.

--- Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question207.m
+++ Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question207.m
@@ -32,6 +32,7 @@
   self.startedOnDate = @"17/04/13";
   self.completedOnDate = @"17/04/13";
   self.solutionLineCount = @"11";
+  self.usesCustomObjects = NO;
   self.usesHelperMethods = NO;
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = NO;
