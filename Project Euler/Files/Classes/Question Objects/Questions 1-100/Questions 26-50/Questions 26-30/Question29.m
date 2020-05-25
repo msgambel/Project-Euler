17415575b7722f7e63abf50f15b4d1608d332df2
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-05-25 05:05:05 -0400

    Added a Uses Custom Structs to Question 29.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question29.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question29.m
@@ -35,6 +35,7 @@
   self.completedOnDate = @"29/01/13";
   self.solutionLineCount = @"45";
   self.usesCustomObjects = YES;
+  self.usesCustomStructs = YES;
   self.usesHelperMethods = NO;
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = YES;
