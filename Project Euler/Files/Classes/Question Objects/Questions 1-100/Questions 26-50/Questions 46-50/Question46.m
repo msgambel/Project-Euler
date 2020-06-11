0cbca4ae0de38a8ee50a4d0812fab6976ad01624
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-06-11 05:05:05 -0400

    Added a Uses Custom Structs to Question 46.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question46.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question46.m
@@ -33,6 +33,7 @@
   self.completedOnDate = @"15/02/13";
   self.solutionLineCount = @"71";
   self.usesCustomObjects = NO;
+  self.usesCustomStructs = NO;
   self.usesHelperMethods = YES;
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = NO;
