86efc1c2377938a0206ae199f746cb9cd59c0bb3
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-06-29 05:05:05 -0400

    Added a Uses Custom Structs to Question 64.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question64.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question64.m
@@ -33,6 +33,7 @@
   self.completedOnDate = @"05/03/13";
   self.solutionLineCount = @"43";
   self.usesCustomObjects = NO;
+  self.usesCustomStructs = NO;
   self.usesHelperMethods = YES;
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = NO;
