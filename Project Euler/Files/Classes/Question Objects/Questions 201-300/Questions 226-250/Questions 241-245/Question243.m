56d6bce11a10b48263879d442c9779ab95a9403a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-08-12 03:03:03 -0400

    Added a Uses Custom Structs to Question 243.

--- Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 226-250/Questions 241-245/Question243.m
+++ Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 226-250/Questions 241-245/Question243.m
@@ -33,6 +33,7 @@
   self.completedOnDate = @"18/04/13";
   self.solutionLineCount = @"47";
   self.usesCustomObjects = NO;
+  self.usesCustomStructs = NO;
   self.usesHelperMethods = YES;
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = YES;
