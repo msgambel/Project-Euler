b7b74121d9b615e268528ba83f57568454769927
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-08-19 03:03:03 -0400

    Added a Solvable By Hand to Question 2.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question2.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question2.m
@@ -30,6 +30,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"02/01/13";
+  self.solvableByHand = YES;
   self.completedOnDate = @"02/01/13";
   self.solutionLineCount = @"11";
   self.usesCustomObjects = NO;
