ea87523f20474a0cbaedba726e6eb5153bdf92ba
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-09-21 03:03:03 -0400

    Added a Solvable By Hand to Question 35.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question35.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question35.m
@@ -37,6 +37,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = YES;
   self.startedOnDate = @"04/02/13";
+  self.solvableByHand = YES;
   self.completedOnDate = @"04/02/13";
   self.solutionLineCount = @"81";
   self.usesCustomObjects = NO;
