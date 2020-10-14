f75896cbe9a65c6f4dfd50c51cd63745e57df79e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-10-14 03:03:03 -0400

    Added a Solvable By Hand to Question 58.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question58.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question58.m
@@ -30,6 +30,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"27/02/13";
+  self.solvableByHand = NO;
   self.completedOnDate = @"27/02/13";
   self.solutionLineCount = @"47";
   self.usesCustomObjects = NO;
