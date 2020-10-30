6f72207b3d98fb3dc31720a1de4989d409fde905
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-10-30 03:03:03 -0400

    Added a Solvable By Hand to Question 74.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question74.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question74.m
@@ -30,6 +30,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"15/03/13";
+  self.solvableByHand = NO;
   self.completedOnDate = @"15/03/13";
   self.solutionLineCount = @"37";
   self.usesCustomObjects = NO;
