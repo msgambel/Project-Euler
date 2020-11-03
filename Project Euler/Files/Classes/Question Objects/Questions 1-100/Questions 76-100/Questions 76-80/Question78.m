bb85d61487b98994469f8013692c4f715b10cdc3
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-11-03 03:03:03 -0500

    Added a Solvable By Hand to Question 78.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question78.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question78.m
@@ -31,6 +31,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"19/03/13";
+  self.solvableByHand = YES;
   self.completedOnDate = @"19/03/13";
   self.solutionLineCount = @"29";
   self.usesCustomObjects = NO;
