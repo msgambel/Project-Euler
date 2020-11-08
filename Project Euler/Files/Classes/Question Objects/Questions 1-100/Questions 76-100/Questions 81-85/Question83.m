0a14ceff9c034d5f8da410611170db6db0e21286
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-11-08 03:03:03 -0500

    Added a Solvable By Hand to Question 83.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question83.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question83.m
@@ -31,6 +31,7 @@
   self.attemptsCount = @"5";
   self.isChallenging = YES;
   self.startedOnDate = @"24/03/13";
+  self.solvableByHand = YES;
   self.completedOnDate = @"24/03/13";
   self.solutionLineCount = @"51";
   self.usesCustomObjects = YES;
