9e6d6f2ec0ae5b0fbce555bba88325bb9f8f06c9
Michael Sgambelluri <michaelsgam10@hotmail.com>
2020-12-22 03:03:03 -0500

    Added a Learned Something New to Question 14.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question14.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question14.m
@@ -36,6 +36,7 @@
   self.usesCustomObjects = NO;
   self.usesCustomStructs = NO;
   self.usesHelperMethods = NO;
+  self.learnedSomethingNew = NO;
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = YES;
   self.estimatedComputationTime = @"0.541";
