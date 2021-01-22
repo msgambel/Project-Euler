f237039a00e60739f70a545bb97e17f91f1aaa6c
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-01-22 03:03:03 -0500

    Added a Learned Something New to Question 45.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question45.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question45.m
@@ -36,6 +36,7 @@
   self.usesCustomObjects = NO;
   self.usesCustomStructs = NO;
   self.usesHelperMethods = YES;
+  self.learnedSomethingNew = NO;
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"8.31e-04";
