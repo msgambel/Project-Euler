68ac67e9a3479d88b8b4640dec5a9a0dd6bda489
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-01-25 03:03:03 -0500

    Added a Learned Something New to Question 48.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question48.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question48.m
@@ -37,6 +37,7 @@
   self.usesCustomObjects = YES;
   self.usesCustomStructs = NO;
   self.usesHelperMethods = YES;
+  self.learnedSomethingNew = NO;
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"0.191";
