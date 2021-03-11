07f23ac04268f8e035645ca803e161c4ea0cce18
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-03-11 03:03:03 -0500

    Added a Learned Something New to Question 93.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question93.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question93.m
@@ -44,6 +44,7 @@
   self.usesCustomObjects = NO;
   self.usesCustomStructs = NO;
   self.usesHelperMethods = YES;
+  self.learnedSomethingNew = YES;
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"7.38";
