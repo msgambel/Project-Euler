1edf3b40a2249689fd4262d27b7f29721b9287ea
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-04-02 03:03:03 -0400

    Added a Should Investigate Further to Question 2.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question2.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question2.m
@@ -40,6 +40,7 @@
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = YES;
   self.estimatedComputationTime = @"2.1e-05";
+  self.shouldInvestigateFurther = YES;
   self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"4.7e-05";
 }
