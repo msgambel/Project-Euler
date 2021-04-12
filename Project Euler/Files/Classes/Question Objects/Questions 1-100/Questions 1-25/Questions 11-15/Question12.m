571f38ca8dadd64526740b5bf8448515bf7fa1ff
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-04-12 03:03:03 -0400

    Added a Should Investigate Further to Question 12.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question12.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question12.m
@@ -40,6 +40,7 @@
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = YES;
   self.estimatedComputationTime = @"1.09e-02";
+  self.shouldInvestigateFurther = NO;
   self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"565";
 }
