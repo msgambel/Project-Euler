c72164e2e4d096a9b839c668703717a5aafda3a6
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-04-28 03:03:03 -0400

    Added a Should Investigate Further to Question 28.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question28.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question28.m
@@ -40,6 +40,7 @@
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = YES;
   self.estimatedComputationTime = @"2.9e-05";
+  self.shouldInvestigateFurther = NO;
   self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"3.1e-05";
 }
