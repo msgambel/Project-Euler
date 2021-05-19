d9d4ff01aab2acf224e5fead80c070bab3ed9a94
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-05-19 03:03:03 -0400

    Added a Should Investigate Further to Question 49.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question49.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question49.m
@@ -40,6 +40,7 @@
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"0.127";
+  self.shouldInvestigateFurther = NO;
   self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"0.127";
 }
