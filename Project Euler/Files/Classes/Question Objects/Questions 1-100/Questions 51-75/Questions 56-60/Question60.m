2627a2abf544b1cfd9b4ad5acf417eb019b48a7e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-05-30 03:03:03 -0400

    Added a Should Investigate Further to Question 60.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question60.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 56-60/Question60.m
@@ -49,6 +49,7 @@
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = YES;
   self.estimatedComputationTime = @"0.289";
+  self.shouldInvestigateFurther = NO;
   self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"1.09";
 }
