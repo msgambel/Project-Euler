f3497de508e2ebb83d98e57a3fb79caeedf7fac7
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-06-02 03:03:03 -0400

    Added a Should Investigate Further to Question 63.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
@@ -40,6 +40,7 @@
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"2.8e-05";
+  self.shouldInvestigateFurther = NO;
   self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"2.8e-05";
 }
