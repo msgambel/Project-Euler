c272bfeb7a701a189cf4c59f23e7bd67a06c4ad7
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-06-20 03:03:03 -0400

    Added a Should Investigate Further to Question 81.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question81.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question81.m
@@ -41,6 +41,7 @@
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"2.73e-02";
+  self.shouldInvestigateFurther = YES;
   self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"2.73e-02";
 }
