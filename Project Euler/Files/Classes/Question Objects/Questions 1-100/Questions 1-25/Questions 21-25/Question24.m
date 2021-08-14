5eccd24cd7ee22480e1181e7ddc8c8d7b5e96a22
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-08-14 03:03:03 -0400

    Added a Can Be Simplified to Question 24.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question24.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 21-25/Question24.m
@@ -31,6 +31,7 @@
   self.isChallenging = NO;
   self.startedOnDate = @"24/01/13";
   self.solvableByHand = YES;
+  self.canBeSimplified = YES;
   self.completedOnDate = @"24/01/13";
   self.solutionLineCount = @"39";
   self.usesCustomObjects = NO;
