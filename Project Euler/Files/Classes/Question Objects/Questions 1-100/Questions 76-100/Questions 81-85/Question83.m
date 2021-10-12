63e7cadcc51dd58e11535863f26147f160a21ad1
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-10-12 03:03:03 -0400

    Added a Can Be Simplified to Question 83.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question83.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question83.m
@@ -32,6 +32,7 @@
   self.isChallenging = YES;
   self.startedOnDate = @"24/03/13";
   self.solvableByHand = YES;
+  self.canBeSimplified = YES;
   self.completedOnDate = @"24/03/13";
   self.solutionLineCount = @"51";
   self.usesCustomObjects = YES;
