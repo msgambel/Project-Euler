75b8d89044f058dc547e15e013dba4382a5b5718
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-11-05 03:03:03 -0400

    Added a Can Be Simplified to Question 207.

--- Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question207.m
+++ Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question207.m
@@ -31,6 +31,7 @@
   self.isChallenging = NO;
   self.startedOnDate = @"17/04/13";
   self.solvableByHand = YES;
+  self.canBeSimplified = NO;
   self.completedOnDate = @"17/04/13";
   self.solutionLineCount = @"11";
   self.usesCustomObjects = NO;
