1059a8f64671b9fca6de192e01b79a8eb34b4bae
Michael Sgambelluri <michaelsgam10@hotmail.com>
2021-11-26 03:03:03 -0500

    Added a Related To Another Question to Question 16.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question16.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question16.m
@@ -42,6 +42,7 @@
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"3.05e-03";
+  self.relatedToAnotherQuestion = NO;
   self.shouldInvestigateFurther = NO;
   self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"3.05e-03";
