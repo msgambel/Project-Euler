72f7dcca7708cf1bf0541cb7720cc6542706ec00
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-01-03 03:03:03 -0500

    Added a Related To Another Question to Question 54.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question54.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question54.m
@@ -42,6 +42,7 @@
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"3.43e-02";
+  self.relatedToAnotherQuestion = NO;
   self.shouldInvestigateFurther = NO;
   self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"3.43e-02";
