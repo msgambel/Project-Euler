4ce1746dfa8380f8ea2caa9e006784ecbae9eec0
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-02-19 03:03:03 -0500

    Added a Related To Another Question to Question 112.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 111-115/Question112.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 111-115/Question112.m
@@ -49,6 +49,7 @@
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
   self.estimatedComputationTime = @"0.289";
+  self.relatedToAnotherQuestion = YES;
   self.shouldInvestigateFurther = NO;
   self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @"0.289";
