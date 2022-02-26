c451e67f3f831c773b33e1542d4b7aa90c614656
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-02-26 03:03:03 -0500

    Added a Related To Another Question to Question 243.

--- Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 226-250/Questions 241-245/Question243.m
+++ Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 226-250/Questions 241-245/Question243.m
@@ -41,6 +41,7 @@
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = YES;
   self.estimatedComputationTime = @"4.5e-05";
+  self.relatedToAnotherQuestion = YES;
   self.shouldInvestigateFurther = NO;
   self.usesFunctionalProgramming = NO;
   self.estimatedBruteForceComputationTime = @">1000";
