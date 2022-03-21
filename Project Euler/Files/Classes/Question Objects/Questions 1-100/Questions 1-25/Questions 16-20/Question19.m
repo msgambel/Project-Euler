86d07e916f65b2a81b94ae060daacba06537b561
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-03-21 03:03:03 -0400

    Added a Education Level to Question 19.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
@@ -31,6 +31,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"19/01/13";
+  self.educationLevel = @"Elementary";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
   self.completedOnDate = @"19/01/13";
