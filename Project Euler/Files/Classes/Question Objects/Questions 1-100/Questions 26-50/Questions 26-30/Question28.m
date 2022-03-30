6be1cac2b8bcb1ba9007c841043c6728cf3164ad
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-03-30 03:03:03 -0400

    Added a Education Level to Question 28.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question28.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question28.m
@@ -30,6 +30,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"28/01/13";
+  self.educationLevel = @"Elementary";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
   self.completedOnDate = @"28/01/13";
