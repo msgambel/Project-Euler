ce7d7171729be4fd51777a8b722dadb1c18901b7
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-04-09 03:03:03 -0400

    Added a Education Level to Question 38.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question38.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question38.m
@@ -30,6 +30,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"07/02/13";
+  self.educationLevel = @"High School";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
   self.completedOnDate = @"07/02/13";
