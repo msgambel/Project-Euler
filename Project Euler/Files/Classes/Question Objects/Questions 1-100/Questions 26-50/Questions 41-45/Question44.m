cbb0747c2fa94926e6d1e3dd9fa00a0126c1f730
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-04-15 03:03:03 -0400

    Added a Education Level to Question 44.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question44.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question44.m
@@ -30,6 +30,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"13/02/13";
+  self.educationLevel = @"High School";
   self.solvableByHand = NO;
   self.canBeSimplified = NO;
   self.completedOnDate = @"13/02/13";
