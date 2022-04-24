0e36e0f1c59097f2b1c23196767d980836b73693
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-04-24 03:03:03 -0400

    Added a Education Level to Question 53.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question53.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question53.m
@@ -31,6 +31,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"22/02/13";
+  self.educationLevel = @"High School";
   self.solvableByHand = NO;
   self.canBeSimplified = NO;
   self.completedOnDate = @"22/02/13";
