b6621964903c00e3a999811291df806a4098f28b
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-05-16 03:03:03 -0400

    Added a Education Level to Question 75.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question75.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question75.m
@@ -30,6 +30,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"16/03/13";
+  self.educationLevel = @"Elementary";
   self.solvableByHand = NO;
   self.canBeSimplified = YES;
   self.completedOnDate = @"16/03/13";
