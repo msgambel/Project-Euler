22eee2baa235a0b0b3e19fd39d3ef488bc566720
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-05-21 03:03:03 -0400

    Added a Education Level to Question 80.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question80.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question80.m
@@ -31,6 +31,7 @@
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.startedOnDate = @"21/03/13";
+  self.educationLevel = @"High School";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
   self.completedOnDate = @"21/03/13";
