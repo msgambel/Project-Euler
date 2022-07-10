a95c1d1d1b9e3c826ec2e3e98e89e0da946fd1e1
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-07-10 03:03:03 -0400

    Added a Is Contest Math to Question 18.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question18.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question18.m
@@ -30,6 +30,7 @@
   self.commentCount = @"41";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
+  self.isContestMath = NO;
   self.startedOnDate = @"18/01/13";
   self.educationLevel = @"High School";
   self.solvableByHand = YES;
