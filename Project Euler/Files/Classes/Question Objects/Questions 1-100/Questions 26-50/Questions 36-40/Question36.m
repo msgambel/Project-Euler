34d512b9834959d08b6417939a612ba70e270af9
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-07-28 03:03:03 -0400

    Added a Is Contest Math to Question 36.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question36.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question36.m
@@ -35,6 +35,7 @@
   self.commentCount = @"38";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
+  self.isContestMath = NO;
   self.startedOnDate = @"05/02/13";
   self.educationLevel = @"High School";
   self.solvableByHand = NO;
