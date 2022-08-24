41f21b7ce9fe2c2b9893524b0b1cc7b5153d90be
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-08-24 03:03:03 -0400

    Added a Is Contest Math to Question 63.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
@@ -29,6 +29,7 @@
   self.commentCount = @"23";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
+  self.isContestMath = NO;
   self.startedOnDate = @"04/03/13";
   self.educationLevel = @"Elementary";
   self.solvableByHand = YES;
