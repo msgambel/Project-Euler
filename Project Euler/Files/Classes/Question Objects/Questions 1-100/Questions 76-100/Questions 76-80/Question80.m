5d6369621bdf3329daddab2da9209b27328e9904
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-09-10 03:03:03 -0400

    Added a Is Contest Math to Question 80.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question80.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question80.m
@@ -30,6 +30,7 @@
   self.commentCount = @"19";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
+  self.isContestMath = NO;
   self.startedOnDate = @"21/03/13";
   self.educationLevel = @"High School";
   self.solvableByHand = YES;
