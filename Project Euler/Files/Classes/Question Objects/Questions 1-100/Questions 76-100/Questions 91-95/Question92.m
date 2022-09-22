4a494f0267b484431308388cfc2adb3dd100ac34
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-09-22 03:03:03 -0400

    Added a Is Contest Math to Question 92.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question92.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question92.m
@@ -35,6 +35,7 @@
   self.commentCount = @"29";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
+  self.isContestMath = NO;
   self.startedOnDate = @"02/04/13";
   self.educationLevel = @"Elementary";
   self.solvableByHand = NO;
