17ffc02dae98007aae832dc150f1e38ccda5ab06
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-10-05 03:03:03 -0400

    Added a Is Contest Math to Question 187.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
@@ -29,6 +29,7 @@
   self.commentCount = @"33";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
+  self.isContestMath = NO;
   self.startedOnDate = @"15/04/13";
   self.educationLevel = @"High School";
   self.solvableByHand = NO;
