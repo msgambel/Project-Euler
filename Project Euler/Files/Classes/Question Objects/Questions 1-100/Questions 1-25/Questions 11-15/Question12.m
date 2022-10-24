a2abc01ca4ec82a1d3db703d847a33a7b9af4b58
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-10-24 03:03:03 -0400

    Added a Trick Required to Question 12.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question12.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question12.m
@@ -31,6 +31,7 @@
   self.isChallenging = NO;
   self.isContestMath = NO;
   self.startedOnDate = @"12/01/13";
+  self.trickRequired = NO;
   self.educationLevel = @"Elementary";
   self.solvableByHand = YES;
   self.canBeSimplified = YES;
