4a2c95f291993a7f0c3be5d42f242595211011dd
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-11-10 03:03:03 -0500

    Added a Trick Required to Question 29.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question29.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question29.m
@@ -33,6 +33,7 @@
   self.isChallenging = NO;
   self.isContestMath = NO;
   self.startedOnDate = @"29/01/13";
+  self.trickRequired = NO;
   self.educationLevel = @"Elementary";
   self.solvableByHand = YES;
   self.canBeSimplified = YES;
