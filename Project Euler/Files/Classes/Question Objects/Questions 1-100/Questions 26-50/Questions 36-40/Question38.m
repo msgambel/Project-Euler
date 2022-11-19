21b5dec89ddc63090d994bb933b3affef4b76b4b
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-11-19 03:03:03 -0500

    Added a Trick Required to Question 38.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question38.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question38.m
@@ -31,6 +31,7 @@
   self.isChallenging = NO;
   self.isContestMath = NO;
   self.startedOnDate = @"07/02/13";
+  self.trickRequired = NO;
   self.educationLevel = @"High School";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
