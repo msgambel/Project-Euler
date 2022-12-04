8cc9372e2864205641908cb90cfa8f360808d123
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-12-04 03:03:03 -0500

    Added a Trick Required to Question 53.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question53.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question53.m
@@ -32,6 +32,7 @@
   self.isChallenging = NO;
   self.isContestMath = NO;
   self.startedOnDate = @"22/02/13";
+  self.trickRequired = NO;
   self.educationLevel = @"High School";
   self.solvableByHand = NO;
   self.canBeSimplified = NO;
