072bd286fdf8e54ab4c79315ec949b0704844edf
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-12-19 03:03:03 -0500

    Added a Trick Required to Question 68.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question68.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question68.m
@@ -31,6 +31,7 @@
   self.isChallenging = YES;
   self.isContestMath = YES;
   self.startedOnDate = @"09/03/13";
+  self.trickRequired = YES;
   self.educationLevel = @"High School";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
