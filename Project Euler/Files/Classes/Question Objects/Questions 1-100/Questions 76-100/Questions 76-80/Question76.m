23e98036d8f00c78b06678c95717d4b744e999ec
Michael Sgambelluri <michaelsgam10@hotmail.com>
2022-12-27 03:03:03 -0500

    Added a Trick Required to Question 76.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question76.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question76.m
@@ -37,6 +37,7 @@
   self.isChallenging = NO;
   self.isContestMath = NO;
   self.startedOnDate = @"17/03/13";
+  self.trickRequired = NO;
   self.educationLevel = @"High School";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
