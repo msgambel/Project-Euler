748f67166f18a5d740ab150a569e53bd604a0454
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-01-04 03:03:03 -0500

    Added a Trick Required to Question 84.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question84.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question84.m
@@ -32,6 +32,7 @@
   self.isChallenging = YES;
   self.isContestMath = NO;
   self.startedOnDate = @"25/03/13";
+  self.trickRequired = NO;
   self.educationLevel = @"High School";
   self.solvableByHand = NO;
   self.canBeSimplified = YES;
