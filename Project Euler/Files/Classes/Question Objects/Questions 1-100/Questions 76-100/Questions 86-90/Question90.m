a44c032e8455305ba0a0a5c6c3fb19d43c447d6d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-01-10 03:03:03 -0500

    Added a Trick Required to Question 90.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question90.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question90.m
@@ -37,6 +37,7 @@
   self.isChallenging = NO;
   self.isContestMath = NO;
   self.startedOnDate = @"31/03/13";
+  self.trickRequired = NO;
   self.educationLevel = @"Elementary";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
