bcd622549709251d088e743b526fcf34eb5277df
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-01-22 03:03:03 -0500

    Added a Trick Required to Question 120.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
@@ -31,6 +31,7 @@
   self.isChallenging = NO;
   self.isContestMath = NO;
   self.startedOnDate = @"12/04/13";
+  self.trickRequired = NO;
   self.educationLevel = @"High School";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
