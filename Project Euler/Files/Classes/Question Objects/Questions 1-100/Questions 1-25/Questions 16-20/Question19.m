60a8ff4da2b95bea38acfc25a8f336de661a0b2b
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-02-20 03:03:03 -0500

    Added a Uses Big Int to Question 19.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
@@ -27,6 +27,7 @@
   self.solveTime = @"60";
   self.technique = @"Recursion";
   self.difficulty = @"Meh";
+  self.usesBigInt = NO;
   self.commentCount = @"32";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
