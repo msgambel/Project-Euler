a78179f7c5780d2998a33b16543f3d48942e2f9d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-03-18 03:03:03 -0400

    Added a Uses Big Int to Question 45.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question45.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question45.m
@@ -26,6 +26,7 @@
   self.solveTime = @"300";
   self.technique = @"Math";
   self.difficulty = @"Easy";
+  self.usesBigInt = NO;
   self.commentCount = @"15";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
