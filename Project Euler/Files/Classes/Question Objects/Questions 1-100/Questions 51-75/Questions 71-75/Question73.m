d25ae6bd5dd949a40f2f73e9c243e4ef4d63a3d3
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-04-15 03:03:03 -0400

    Added a Uses Big Int to Question 73.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question73.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question73.m
@@ -27,6 +27,7 @@
   self.solveTime = @"300";
   self.technique = @"Math";
   self.difficulty = @"Medium";
+  self.usesBigInt = NO;
   self.commentCount = @"64";
   self.attemptsCount = @"3";
   self.isChallenging = YES;
