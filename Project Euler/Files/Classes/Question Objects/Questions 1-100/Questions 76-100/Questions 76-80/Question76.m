e91f15646c03483f89257a830d2790c24a0c4b25
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-04-18 03:03:03 -0400

    Added a Uses Big Int to Question 76.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question76.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question76.m
@@ -32,6 +32,7 @@
   self.solveTime = @"30";
   self.technique = @"Math";
   self.difficulty = @"Easy";
+  self.usesBigInt = NO;
   self.commentCount = @"13";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
