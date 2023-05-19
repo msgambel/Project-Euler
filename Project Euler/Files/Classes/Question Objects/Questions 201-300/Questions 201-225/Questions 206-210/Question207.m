59b34731f61009c53b98ed8cbecfabccccb00628
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-05-19 03:03:03 -0400

    Added a Uses Big Int to Question 207.

--- Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question207.m
+++ Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 201-225/Questions 206-210/Question207.m
@@ -26,6 +26,7 @@
   self.solveTime = @"60";
   self.technique = @"Math";
   self.difficulty = @"Easy";
+  self.usesBigInt = NO;
   self.commentCount = @"19";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
