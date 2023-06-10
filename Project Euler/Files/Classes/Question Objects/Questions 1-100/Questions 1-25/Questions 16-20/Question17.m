d5da8333e4b6ba8157cc3f8bc8cf2d5d56085558
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-06-10 03:03:03 -0400

    Added a Is Useful to Question 17.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question17.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question17.m
@@ -30,6 +30,7 @@
   self.number = @"17";
   self.rating = @"4";
   self.category = @"Counting";
+  self.isUseful = NO;
   self.keywords = @"letters,sum,words,numbers,counts,british,usage,written,one,thousand,1000,contains,spaces,hyphens,compliance";
   self.solveTime = @"90";
   self.technique = @"Recursion";
