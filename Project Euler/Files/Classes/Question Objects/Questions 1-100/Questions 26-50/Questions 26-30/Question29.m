46a37b24682653f5d5a87ecb8452e65e8c1e8215
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-06-22 03:03:03 -0400

    Added a Is Useful to Question 29.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question29.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question29.m
@@ -24,6 +24,7 @@
   self.number = @"29";
   self.rating = @"3";
   self.category = @"Primes";
+  self.isUseful = YES;
   self.keywords = @"base,power,unique,integer,combinations,numerical,order,distinct,sequence,repeats,removed,terms,generated,pairs,one,hundred,100";
   self.solveTime = @"60";
   self.technique = @"Math";
