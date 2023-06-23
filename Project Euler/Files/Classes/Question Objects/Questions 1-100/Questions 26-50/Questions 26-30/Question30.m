573efab06a0b0427540298bd57c51218245519ba
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-06-23 03:03:03 -0400

    Added a Is Useful to Question 30.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question30.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question30.m
@@ -28,6 +28,7 @@
   self.number = @"30";
   self.rating = @"3";
   self.category = @"Sums";
+  self.isUseful = NO;
   self.keywords = @"sum,powers,digits,fourth,fifth,written,total,200000,two,hundred,thousand,5,five";
   self.solveTime = @"60";
   self.technique = @"Recursion";
