206e5b5ddbf4318c260ac00894c50bfb2354d692
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-07-10 03:03:03 -0400

    Added a Is Useful to Question 47.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question47.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question47.m
@@ -22,6 +22,7 @@
   self.number = @"47";
   self.rating = @"4";
   self.category = @"Primes";
+  self.isUseful = NO;
   self.keywords = @"consecutive,four,4,distinct,prime,factors,numbers,first,integers,maximum,size";
   self.solveTime = @"120";
   self.technique = @"Recursion";
