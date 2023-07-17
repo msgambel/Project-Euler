d33be51e4f40b3c0fa3792d530554b5d14071215
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-07-17 03:03:03 -0400

    Added a Is Useful to Question 54.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question54.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question54.m
@@ -23,6 +23,7 @@
   self.number = @"54";
   self.rating = @"5";
   self.category = @"Patterns";
+  self.isUseful = YES;
   self.keywords = @"poker,hands,two,player,import,rank,compare,5,five,clear,winner,specific,order";
   self.solveTime = @"300";
   self.technique = @"OOP";
