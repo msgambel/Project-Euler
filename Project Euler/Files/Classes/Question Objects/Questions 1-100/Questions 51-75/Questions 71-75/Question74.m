5470f243c1bcfa1e7c943286be71ecc11264751d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-08-06 03:03:03 -0400

    Added a Is Useful to Question 74.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question74.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question74.m
@@ -22,6 +22,7 @@
   self.number = @"74";
   self.rating = @"4";
   self.category = @"Patterns";
+  self.isUseful = NO;
   self.keywords = @"digit,factorial,chains,length,loop,sixty,60,unique,property,non-repeating,terms,contain,exactly";
   self.solveTime = @"30";
   self.technique = @"Recursion";
