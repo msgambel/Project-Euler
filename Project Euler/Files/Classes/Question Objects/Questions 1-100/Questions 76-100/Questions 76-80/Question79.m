4c7c8f3db7a1686ac3bd86e67d31ef3fe42ebb0f
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-08-11 03:03:03 -0400

    Added a Is Useful to Question 79.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question79.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question79.m
@@ -28,6 +28,7 @@
   self.number = @"79";
   self.rating = @"5";
   self.category = @"Patterns";
+  self.isUseful = YES;
   self.keywords = @"passcode,derivation,login,secret,shortest,import,security,common,method,analyse,files";
   self.solveTime = @"300";
   self.technique = @"Recursion";
