1dc6f30309f5c4ce245df2a356d82b1d0b875bf2
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-09-24 03:03:03 -0400

    Added a Loads File to Question 11.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question11.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question11.m
@@ -31,6 +31,7 @@
   self.category = @"Combinations";
   self.isUseful = NO;
   self.keywords = @"product,direction,largest,grid,up,down,left,right,diagonal,four,4,adjacent,numbers,20,twenty,dimensional,array,greatest,in";
+  self.loadsFile = NO;
   self.solveTime = @"90";
   self.technique = @"Recursion";
   self.difficulty = @"Meh";
