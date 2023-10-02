1815e75a9dcd8fa5aa36c278044b1de299c937e5
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-10-02 03:03:03 -0400

    Added a Loads File to Question 19.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
@@ -25,6 +25,7 @@
   self.category = @"Counting";
   self.isUseful = YES;
   self.keywords = @"week,month,day,counting,leap,year,twentieth,century,sundays,first,sum,total,research,divisible,fell";
+  self.loadsFile = NO;
   self.solveTime = @"60";
   self.technique = @"Recursion";
   self.difficulty = @"Meh";
