34a2c799274d06c5101fec0a1750fcb34f3a5b92
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-10-20 03:03:03 -0400

    Added a Loads File to Question 37.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question37.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question37.m
@@ -33,6 +33,7 @@
   self.category = @"Primes";
   self.isUseful = NO;
   self.keywords = @"primes,left,right,truncatable,continuously,remove,digits,7,seven,sum,only,hash,table";
+  self.loadsFile = NO;
   self.solveTime = @"180";
   self.technique = @"Recursion";
   self.difficulty = @"Medium";
