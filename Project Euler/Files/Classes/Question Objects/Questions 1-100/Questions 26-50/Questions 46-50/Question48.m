ae62eaed2d309dad7895254dbb6d32f9423bb2fa
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-10-31 03:03:03 -0400

    Added a Loads File to Question 48.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question48.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question48.m
@@ -25,6 +25,7 @@
   self.category = @"Patterns";
   self.isUseful = YES;
   self.keywords = @"sum,powers,series,ten,10,last,digits,self,modular,exponentiation,bigint,radix,find,modulo,1000";
+  self.loadsFile = NO;
   self.solveTime = @"30";
   self.technique = @"Math";
   self.difficulty = @"Easy";
