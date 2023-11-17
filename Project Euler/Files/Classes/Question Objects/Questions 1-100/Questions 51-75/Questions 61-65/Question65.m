c80d7280dee9326c66a9467267309b6b12722dec
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-11-17 03:03:03 -0500

    Added a Loads File to Question 65.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question65.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question65.m
@@ -25,6 +25,7 @@
   self.category = @"Patterns";
   self.isUseful = YES;
   self.keywords = @"convergents,e,infinite,continued,fractions,digits,sum,numerator,100,one,hundred,rational,approximations,sequence,partial";
+  self.loadsFile = NO;
   self.solveTime = @"30";
   self.technique = @"Recursion";
   self.difficulty = @"Medium";
