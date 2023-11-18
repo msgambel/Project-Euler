21fb9098828b84ce1ce63fbd911f9200467d4037
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-11-18 03:03:03 -0500

    Added a Loads File to Question 66.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question66.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question66.m
@@ -24,6 +24,7 @@
   self.category = @"Patterns";
   self.isUseful = YES;
   self.keywords = @"diophantine,equations,continued,fractions,expansions,pells,periods,minimal,solutions,largest,value,quadratic,length";
+  self.loadsFile = NO;
   self.solveTime = @"300";
   self.technique = @"Math";
   self.difficulty = @"Medium";
