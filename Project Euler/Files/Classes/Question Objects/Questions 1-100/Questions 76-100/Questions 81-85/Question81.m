fedbc158fb0917c2f2c0d963f8c765d68423f90c
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-12-03 03:03:03 -0500

    Added a Loads File to Question 81.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question81.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question81.m
@@ -25,6 +25,7 @@
   self.category = @"Combinations";
   self.isUseful = YES;
   self.keywords = @"matrix,a*,a,star,minimal,path,sum,import,80,eighty,2,two,ways,files,matrix";
+  self.loadsFile = YES;
   self.solveTime = @"60";
   self.technique = @"OOP";
   self.difficulty = @"Easy";
