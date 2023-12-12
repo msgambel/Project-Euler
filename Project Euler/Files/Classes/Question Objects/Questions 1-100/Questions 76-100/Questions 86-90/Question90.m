1b989da0e241ad9193bcb1318fc823eb6d435b7a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2023-12-12 03:03:03 -0500

    Added a Loads File to Question 90.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question90.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question90.m
@@ -30,6 +30,7 @@
   self.category = @"Combinations";
   self.isUseful = NO;
   self.keywords = @"cube,digit,pairs,distinct,formations,arrangements,square,numbers,different,positions,side,6,six,2,two,displayed,forming";
+  self.loadsFile = NO;
   self.solveTime = @"300";
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
