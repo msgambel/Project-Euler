19caf9f68c2b43543a15cd00cc645fd249b57b8a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2024-01-10 03:03:03 -0500

    Added a Recommended to Question 7.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question7.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question7.m
@@ -29,6 +29,7 @@
   self.technique = @"Math";
   self.difficulty = @"Meh";
   self.usesBigInt = NO;
+  self.recommended = YES;
   self.commentCount = @"10";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
