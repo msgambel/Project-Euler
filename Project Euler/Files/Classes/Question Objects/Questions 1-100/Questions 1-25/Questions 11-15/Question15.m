697e0dc3e8a5835e731887f1dd8b0a2e3f35610d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2024-01-18 03:03:03 -0500

    Added a Recommended to Question 15.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question15.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 11-15/Question15.m
@@ -36,6 +36,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Meh";
   self.usesBigInt = NO;
+  self.recommended = YES;
   self.commentCount = @"19";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
