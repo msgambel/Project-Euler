5259f761ea52f05c8b138b97911d967bb084f807
Michael Sgambelluri <michaelsgam10@hotmail.com>
2024-02-08 03:03:03 -0500

    Added a Recommended to Question 36.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question36.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question36.m
@@ -35,6 +35,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
   self.usesBigInt = NO;
+  self.recommended = NO;
   self.commentCount = @"38";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
