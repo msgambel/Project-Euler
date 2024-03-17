b2fac960e2fc011c8ee511a607d124352ae4da2b
Michael Sgambelluri <michaelsgam10@hotmail.com>
2024-03-17 03:03:03 -0400

    Added a Recommended to Question 74.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question74.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question74.m
@@ -29,6 +29,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
   self.usesBigInt = NO;
+  self.recommended = YES;
   self.commentCount = @"34";
   self.attemptsCount = @"1";
   self.isChallenging = NO;
