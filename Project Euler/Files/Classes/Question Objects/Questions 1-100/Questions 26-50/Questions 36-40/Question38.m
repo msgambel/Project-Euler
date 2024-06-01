265d1c41657ead896eb84577b6fac80073f0e0ea
Michael Sgambelluri <michaelsgam10@hotmail.com>
2024-06-01 03:03:03 -0400

    Added a Uses Recursion to Question 38.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question38.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question38.m
@@ -36,6 +36,7 @@
   self.isContestMath = NO;
   self.startedOnDate = @"07/02/13";
   self.trickRequired = NO;
+  self.usesRecursion = YES;
   self.educationLevel = @"High School";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
