58b40ab5298d699b22fe5feab7ea17a8b3feb633
Michael Sgambelluri <michaelsgam10@hotmail.com>
2024-06-08 03:03:03 -0400

    Added a Uses Recursion to Question 45.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question45.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question45.m
@@ -36,6 +36,7 @@
   self.isContestMath = NO;
   self.startedOnDate = @"14/02/13";
   self.trickRequired = NO;
+  self.usesRecursion = YES;
   self.educationLevel = @"High School";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
