cfc5b24b54ec6c5dfc4f510becd8b7273f805e3b
Michael Sgambelluri <michaelsgam10@hotmail.com>
2024-07-10 03:03:03 -0400

    Added a Uses Recursion to Question 77.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question77.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question77.m
@@ -36,6 +36,7 @@
   self.isContestMath = NO;
   self.startedOnDate = @"18/03/13";
   self.trickRequired = NO;
+  self.usesRecursion = YES;
   self.educationLevel = @"High School";
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
