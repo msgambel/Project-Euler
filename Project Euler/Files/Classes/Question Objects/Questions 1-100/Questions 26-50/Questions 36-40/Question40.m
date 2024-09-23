57f2e733bfdd574ab2430d8c6aa0175a8270baba
Michael Sgambelluri <michaelsgam10@hotmail.com>
2024-09-23 03:03:03 -0400

    Added a Summary to Question 40.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question40.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question40.m
@@ -21,6 +21,7 @@
   self.answer = @"210";
   self.number = @"40";
   self.rating = @"4";
+  self.summary = @"Concatenate the integers, find the digit for each power of 10 from 1 to 6, and multiply them.";
   self.category = @"Combinations";
   self.isUseful = YES;
   self.keywords = @"concatenate,digits,constant,champernowne's,positive,integers,expression,fractional,part,number,value";
