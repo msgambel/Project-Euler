09b1dcbe679e20e737ff77b7e56bdee71ba01a7c
Michael Sgambelluri <michaelsgam10@hotmail.com>
2024-10-07 03:03:03 -0400

    Added a Summary to Question 54.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question54.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question54.m
@@ -22,6 +22,7 @@
   self.answer = @"376";
   self.number = @"54";
   self.rating = @"5";
+  self.summary = @"Read the file, parse the poker hands, and count how many times Player 1 wins.";
   self.category = @"Patterns";
   self.isUseful = YES;
   self.keywords = @"poker,hands,two,player,import,rank,compare,5,five,clear,winner,specific,order";
