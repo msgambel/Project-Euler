ec8f17204c08f59ddcb837c8240fd04bc9174a60
Michael Sgambelluri <michaelsgam10@hotmail.com>
2024-11-09 03:03:03 -0500

    Added a Summary to Question 87.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question87.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question87.m
@@ -21,6 +21,7 @@
   self.answer = @"1097343";
   self.number = @"87";
   self.rating = @"4";
+  self.summary = @"How many numbers < 50,000,000 are the sum of a prime square, cube, and fourth power?";
   self.category = @"Sums";
   self.isUseful = NO;
   self.keywords = @"prime,square,cube,fourth,power,sum,count,triples,count,50000000,fifty,million,below,expressed,numbers";
