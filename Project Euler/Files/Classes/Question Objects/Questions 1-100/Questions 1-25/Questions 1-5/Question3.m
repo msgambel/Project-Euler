4f4dc157fc654d720fa640c3b99c72bd65905933
Michael Sgambelluri <michaelsgam10@hotmail.com>
2024-12-07 03:03:03 -0500

    Added a Memorable to Question 3.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question3.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 1-5/Question3.m
@@ -26,6 +26,7 @@
   self.isUseful = YES;
   self.keywords = @"prime,factors,largest,natural,numbers,sieve,maximum,square,root";
   self.loadsFile = NO;
+  self.memorable = NO;
   self.solveTime = @"30";
   self.technique = @"Recursion";
   self.difficulty = @"Meh";
