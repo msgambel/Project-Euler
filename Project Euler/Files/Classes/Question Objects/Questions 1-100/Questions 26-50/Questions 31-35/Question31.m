5a63732684bf67fe586d5ae3f0da5b0b48ee0673
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-01-04 03:03:03 -0500

    Added a Memorable to Question 31.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question31.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question31.m
@@ -34,6 +34,7 @@
   self.isUseful = YES;
   self.keywords = @"pounds,coins,sums,currency,different,unique,ways,two,2,number,combinations,England,pence,general,circulation";
   self.loadsFile = NO;
+  self.memorable = NO;
   self.solveTime = @"150";
   self.technique = @"Functional";
   self.difficulty = @"Medium";
