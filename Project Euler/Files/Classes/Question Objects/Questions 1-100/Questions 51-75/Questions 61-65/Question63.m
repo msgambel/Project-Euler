82fc02f9b8efb49efc9a6a65082cedf2c34708f4
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-02-05 03:03:03 -0500

    Added a Memorable to Question 63.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
@@ -26,6 +26,7 @@
   self.isUseful = NO;
   self.keywords = @"powers,digits,count,fifth,nine,9,exist,positive,integers,number,nth,bases,exponents";
   self.loadsFile = NO;
+  self.memorable = NO;
   self.solveTime = @"30";
   self.technique = @"Math";
   self.difficulty = @"Easy";
