91c77892f7638f34ba0d1f59d3a194e0bc818e3d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-02-25 03:03:03 -0500

    Added a Memorable to Question 83.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question83.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question83.m
@@ -27,6 +27,7 @@
   self.isUseful = YES;
   self.keywords = @"matrix,a*,a,star,minimal,path,sum,import,moving,up,down";
   self.loadsFile = YES;
+  self.memorable = YES;
   self.solveTime = @"900";
   self.technique = @"OOP";
   self.difficulty = @"Medium";
