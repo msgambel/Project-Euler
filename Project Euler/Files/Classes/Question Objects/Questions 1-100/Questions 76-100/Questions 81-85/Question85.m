e4194fb46d6de1e28eaad025de460531bafbd3b0
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-02-27 03:03:03 -0500

    Added a Memorable to Question 85.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question85.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 81-85/Question85.m
@@ -27,6 +27,7 @@
   self.isUseful = YES;
   self.keywords = @"rectangles,grid,size,counting,triangle,numbers,2000000,two,million,nearest,solution,area,sub";
   self.loadsFile = NO;
+  self.memorable = NO;
   self.solveTime = @"300";
   self.technique = @"Math";
   self.difficulty = @"Medium";
