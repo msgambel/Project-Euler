3c18f7ed39b5e3256539d623e8899e12799d7861
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-04-05 03:03:03 -0400

    Added a Worth Revisiting to Question 10.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question10.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 6-10/Question10.m
@@ -43,6 +43,7 @@
   self.solvableByHand = NO;
   self.canBeSimplified = NO;
   self.completedOnDate = @"10/01/13";
+  self.worthRevisiting = NO;
   self.solutionLineCount = @"31";
   self.usesCustomObjects = NO;
   self.usesCustomStructs = NO;
