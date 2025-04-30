61f0bd3235395fdee75e8a4be51dd4ca92616fa3
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-04-30 03:03:03 -0400

    Added a Worth Revisiting to Question 35.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question35.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 31-35/Question35.m
@@ -50,6 +50,7 @@
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
   self.completedOnDate = @"04/02/13";
+  self.worthRevisiting = NO;
   self.solutionLineCount = @"81";
   self.usesCustomObjects = NO;
   self.usesCustomStructs = NO;
