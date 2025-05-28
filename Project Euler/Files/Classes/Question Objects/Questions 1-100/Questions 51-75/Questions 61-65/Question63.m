15e76c364efda0cc4456f01e9572905c6d486c52
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-05-28 03:03:03 -0400

    Added a Worth Revisiting to Question 63.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
@@ -43,6 +43,7 @@
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
   self.completedOnDate = @"04/03/13";
+  self.worthRevisiting = NO;
   self.solutionLineCount = @"17";
   self.usesCustomObjects = NO;
   self.usesCustomStructs = NO;
