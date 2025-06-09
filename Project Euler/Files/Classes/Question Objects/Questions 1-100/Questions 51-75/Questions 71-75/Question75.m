6f6cafed1d18c8990c1c02c6f35274523895229a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-06-09 03:03:03 -0400

    Added a Worth Revisiting to Question 75.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question75.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 71-75/Question75.m
@@ -43,6 +43,7 @@
   self.solvableByHand = NO;
   self.canBeSimplified = YES;
   self.completedOnDate = @"16/03/13";
+  self.worthRevisiting = NO;
   self.solutionLineCount = @"99";
   self.usesCustomObjects = NO;
   self.usesCustomStructs = NO;
