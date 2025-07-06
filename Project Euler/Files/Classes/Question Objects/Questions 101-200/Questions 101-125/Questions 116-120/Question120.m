d78285a904611d5b7ffa056587b54ec806364071
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-07-06 03:03:03 -0400

    Added a Worth Revisiting to Question 120.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
@@ -43,6 +43,7 @@
   self.solvableByHand = YES;
   self.canBeSimplified = NO;
   self.completedOnDate = @"12/04/13";
+  self.worthRevisiting = NO;
   self.solutionLineCount = @"9";
   self.usesCustomObjects = NO;
   self.usesCustomStructs = NO;
