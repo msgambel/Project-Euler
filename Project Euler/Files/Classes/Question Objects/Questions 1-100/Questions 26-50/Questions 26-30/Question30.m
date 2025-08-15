d7ce573a0d24ffe45d9ba38778069e21ea515a1a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-08-15 03:03:03 -0400

    Added a Is Intuitive to Question 30.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question30.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 26-30/Question30.m
@@ -37,6 +37,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
   self.usesBigInt = NO;
+  self.isIntuitive = YES;
   self.recommended = NO;
   self.commentCount = @"12";
   self.attemptsCount = @"1";
