b5bb1cb2961ac72d8e60579db9cb7aea73ec8a5c
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-08-26 03:03:03 -0400

    Added a Is Intuitive to Question 41.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question41.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question41.m
@@ -32,6 +32,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Medium";
   self.usesBigInt = YES;
+  self.isIntuitive = YES;
   self.recommended = YES;
   self.commentCount = @"17";
   self.attemptsCount = @"1";
