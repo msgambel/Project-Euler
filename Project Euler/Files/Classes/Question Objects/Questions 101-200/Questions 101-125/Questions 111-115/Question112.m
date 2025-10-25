b1c52e7d4004d4eddca26dc2e4e0e535f6da813e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-10-25 03:03:03 -0400

    Added a Is Intuitive to Question 112.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 111-115/Question112.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 111-115/Question112.m
@@ -39,6 +39,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Medium";
   self.usesBigInt = NO;
+  self.isIntuitive = YES;
   self.recommended = YES;
   self.commentCount = @"15";
   self.attemptsCount = @"1";
