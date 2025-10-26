df3bd7ad10da956e05d14cdfc6896998fb7dfd6e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-10-26 03:03:03 -0400

    Added a Is Intuitive to Question 120.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
@@ -31,6 +31,7 @@
   self.technique = @"Math";
   self.difficulty = @"Easy";
   self.usesBigInt = NO;
+  self.isIntuitive = YES;
   self.recommended = YES;
   self.commentCount = @"16";
   self.attemptsCount = @"1";
