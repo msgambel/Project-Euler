b2e101fd852d3b6ecbbc6d941ce67af76741e81e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-10-29 03:03:03 -0400

    Added a Is Intuitive to Question 187.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
@@ -31,6 +31,7 @@
   self.technique = @"Recursion";
   self.difficulty = @"Easy";
   self.usesBigInt = NO;
+  self.isIntuitive = YES;
   self.recommended = YES;
   self.commentCount = @"33";
   self.attemptsCount = @"1";
