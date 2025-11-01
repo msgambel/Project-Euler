7879dc2747e26e6e901ffd7b050c2ae57e386cf9
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-11-01 03:03:03 -0400

    Added a Is Intuitive to Question 243.

--- Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 226-250/Questions 241-245/Question243.m
+++ Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 226-250/Questions 241-245/Question243.m
@@ -31,6 +31,7 @@
   self.technique = @"Math";
   self.difficulty = @"Medium";
   self.usesBigInt = NO;
+  self.isIntuitive = YES;
   self.recommended = YES;
   self.commentCount = @"20";
   self.attemptsCount = @"3";
