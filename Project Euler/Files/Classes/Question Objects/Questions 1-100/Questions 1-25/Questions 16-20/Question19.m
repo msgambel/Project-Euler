c23789d9a7dec98e9ae6c64b3055cfd139c693d1
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-11-24 03:03:03 -0500

    Added a Solution Works In General to Question 19.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question19.m
@@ -53,6 +53,7 @@
   self.learnedSomethingNew = NO;
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
+  self.solutionWorksInGeneral = YES;
   self.estimatedComputationTime = @"1.86e-04";
   self.relatedToAnotherQuestion = NO;
   self.shouldInvestigateFurther = YES;
