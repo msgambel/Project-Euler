48893c59a3076c0381fbedc947cc434faace15ea
Michael Sgambelluri <michaelsgam10@hotmail.com>
2025-12-14 03:03:03 -0500

    Added a Solution Works In General to Question 39.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question39.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 36-40/Question39.m
@@ -52,6 +52,7 @@
   self.learnedSomethingNew = NO;
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
+  self.solutionWorksInGeneral = YES;
   self.estimatedComputationTime = @"2.43e-03";
   self.relatedToAnotherQuestion = YES;
   self.shouldInvestigateFurther = NO;
