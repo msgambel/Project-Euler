36a3fdb0f90c5b2706816359b90add86db848ef3
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-01-11 03:03:03 -0500

    Added a Solution Works In General to Question 67.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question67.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 66-70/Question67.m
@@ -53,6 +53,7 @@
   self.learnedSomethingNew = NO;
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
+  self.solutionWorksInGeneral = YES;
   self.estimatedComputationTime = @"1.23e-02";
   self.relatedToAnotherQuestion = NO;
   self.shouldInvestigateFurther = YES;
