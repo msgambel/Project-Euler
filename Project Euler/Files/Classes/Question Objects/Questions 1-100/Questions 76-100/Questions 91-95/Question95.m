ad26444376d6c6d96fb60dbe832b9d9044227d93
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-02-08 03:03:03 -0500

    Added a Solution Works In General to Question 95.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question95.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 91-95/Question95.m
@@ -53,6 +53,7 @@
   self.learnedSomethingNew = YES;
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
+  self.solutionWorksInGeneral = YES;
   self.estimatedComputationTime = @"10.1";
   self.relatedToAnotherQuestion = YES;
   self.shouldInvestigateFurther = YES;
