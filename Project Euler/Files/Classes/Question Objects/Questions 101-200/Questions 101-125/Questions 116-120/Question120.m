c2634a8ab179a66c8a2c98260b33e3be0efdbaa3
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-02-15 03:03:03 -0500

    Added a Solution Works In General to Question 120.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 101-125/Questions 116-120/Question120.m
@@ -52,6 +52,7 @@
   self.learnedSomethingNew = NO;
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = NO;
+  self.solutionWorksInGeneral = YES;
   self.estimatedComputationTime = @"2.9e-05";
   self.relatedToAnotherQuestion = YES;
   self.shouldInvestigateFurther = NO;
