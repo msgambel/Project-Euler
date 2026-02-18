f2cc2b01abbd83d9a74c0b1ddd55345644e8063e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-02-18 03:03:03 -0500

    Added a Solution Works In General to Question 187.

--- Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
+++ Project Euler/Files/Classes/Question Objects/Questions 101-200/Questions 176-200/Questions 186-190/Question187.m
@@ -52,6 +52,7 @@
   self.learnedSomethingNew = NO;
   self.requiresMathematics = NO;
   self.hasMultipleSolutions = NO;
+  self.solutionWorksInGeneral = YES;
   self.estimatedComputationTime = @"91.5";
   self.relatedToAnotherQuestion = YES;
   self.shouldInvestigateFurther = NO;
