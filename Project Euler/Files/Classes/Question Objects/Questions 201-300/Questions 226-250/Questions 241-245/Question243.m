b5838bb6d6ba01a8e6f3da1ea4c424e32d61ec4e
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-02-21 03:03:03 -0500

    Added a Solution Works In General to Question 243.

--- Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 226-250/Questions 241-245/Question243.m
+++ Project Euler/Files/Classes/Question Objects/Questions 201-300/Questions 226-250/Questions 241-245/Question243.m
@@ -52,6 +52,7 @@
   self.learnedSomethingNew = NO;
   self.requiresMathematics = YES;
   self.hasMultipleSolutions = YES;
+  self.solutionWorksInGeneral = YES;
   self.estimatedComputationTime = @"4.5e-05";
   self.relatedToAnotherQuestion = YES;
   self.shouldInvestigateFurther = NO;
