c6352814312c52d2e9a2c3c55e38c15abb16491c
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-04-14 03:03:03 -0400

    Added a Is Clear to Question 48.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question48.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question48.m
@@ -22,6 +22,7 @@
   self.answer = @"9110846700";
   self.number = @"48";
   self.rating = @"4";
+  self.isClear = YES;
   self.summary = @"Sum n^n, n in [1, 1000], and modulo by 10^10.";
   self.category = @"Patterns";
   self.isUseful = YES;
