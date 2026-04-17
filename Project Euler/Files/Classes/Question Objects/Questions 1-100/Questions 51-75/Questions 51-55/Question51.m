47152afa82bb9d6c27116afea274ee4ae3477136
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-04-17 03:03:03 -0400

    Added a Is Clear to Question 51.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question51.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 51-55/Question51.m
@@ -21,6 +21,7 @@
   self.answer = @"121313";
   self.number = @"51";
   self.rating = @"5";
+  self.isClear = YES;
   self.summary = @"Find an 8 digit prime family related by replacing part of the number with the same digit.";
   self.category = @"Primes";
   self.isUseful = NO;
