7ea3b10eab758e677fa9592d9d371e597841ee45
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-04-29 03:03:03 -0400

    Added a Is Clear to Question 63.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 51-75/Questions 61-65/Question63.m
@@ -21,6 +21,7 @@
   self.answer = @"49";
   self.number = @"63";
   self.rating = @"4";
+  self.isClear = YES;
   self.summary = @"How many n-digit positive integers are an nth power?";
   self.category = @"Counting";
   self.isUseful = NO;
