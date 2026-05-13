c073eae746dac33450c0b25cea1ce5b7b6d294fe
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-05-13 03:03:03 -0400

    Added a Is Clear to Question 77.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question77.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 76-80/Question77.m
@@ -21,6 +21,7 @@
   self.answer = @"71";
   self.number = @"77";
   self.rating = @"5";
+  self.isClear = YES;
   self.summary = @"Find the smallest integer that can be written as the sum of primes in over 5,000 unique ways.";
   self.category = @"Primes";
   self.isUseful = YES;
