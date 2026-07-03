a04021fb4a4748bc81de7afee7c770425d29a311
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-07-03 03:03:03 -0400

    Added a Relevant To CS to Question 16.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question16.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 1-25/Questions 16-20/Question16.m
@@ -36,6 +36,7 @@
   self.isIntuitive = YES;
   self.recommended = YES;
   self.commentCount = @"15";
+  self.relevantToCS = YES;
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.isContestMath = NO;
