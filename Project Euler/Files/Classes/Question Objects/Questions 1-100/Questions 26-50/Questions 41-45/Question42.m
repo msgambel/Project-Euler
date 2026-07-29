65820f7a2baa7bbf769b5095432f44070c70546a
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-07-29 03:03:03 -0400

    Added a Relevant To CS to Question 42.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question42.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 41-45/Question42.m
@@ -36,6 +36,7 @@
   self.isIntuitive = YES;
   self.recommended = NO;
   self.commentCount = @"33";
+  self.relevantToCS = YES;
   self.attemptsCount = @"1";
   self.isChallenging = NO;
   self.isContestMath = NO;
