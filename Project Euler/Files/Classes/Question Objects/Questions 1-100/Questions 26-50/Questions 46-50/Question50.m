9584b1873631c6a0424a7f382de4601a2065f12d
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-08-06 03:03:03 -0400

    Added a Relevant To CS to Question 50.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question50.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 26-50/Questions 46-50/Question50.m
@@ -35,6 +35,7 @@
   self.isIntuitive = YES;
   self.recommended = YES;
   self.commentCount = @"46";
+  self.relevantToCS = YES;
   self.attemptsCount = @"1";
   self.isChallenging = YES;
   self.isContestMath = NO;
