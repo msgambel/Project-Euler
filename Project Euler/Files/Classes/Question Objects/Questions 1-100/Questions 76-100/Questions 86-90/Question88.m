a3b97e79e26acc71f43344123bc5f7059d3154b8
Michael Sgambelluri <michaelsgam10@hotmail.com>
2026-09-13 03:03:03 -0400

    Added a Relevant To CS to Question 88.

--- Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question88.m
+++ Project Euler/Files/Classes/Question Objects/Questions 1-100/Questions 76-100/Questions 86-90/Question88.m
@@ -35,6 +35,7 @@
   self.isIntuitive = NO;
   self.recommended = NO;
   self.commentCount = @"51";
+  self.relevantToCS = YES;
   self.attemptsCount = @"3";
   self.isChallenging = YES;
   self.isContestMath = NO;
